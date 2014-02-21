55ecdd8cb90aa3085e89086af327cf9c6f7d8336
BadButton <kih@hestad.no>
2014-02-21 05:12:06 +0100

    json2db file analyze ver 1

--- WotDBUpdater/json2db.cs
+++ WotDBUpdater/json2db.cs
@@ -16,7 +16,25 @@ namespace WotDBUpdater
 {
     class json2db
     {
-       
+        private static class SectionType
+        {
+            public static string header = "header";
+            public static string tanks = "tanks";
+            public static string tanks_v2 = "tanks_v2";
+        }
+
+        private class TankData
+        {
+            public string tankName = "";
+            public int battleCount = 0;
+
+            public void Clear()
+            {
+                tankName = "";
+                battleCount = 0;
+            }
+        }
+        
         public static String readJson(string filename)
         {
             StringBuilder sb = new StringBuilder();
@@ -33,101 +51,119 @@ namespace WotDBUpdater
 
             string json = sb.ToString();
 
-            try
-            {
-                ConfigData conf = new ConfigData();
-                conf = Config.GetConfig();
-                SqlConnection con = new SqlConnection(conf.DatabaseConn);
-                con.Open();
-                SqlCommand cmd = new SqlCommand("INSERT INTO json (jsonId, jsonString) values (1, @json)", con);
-                cmd.Parameters.AddWithValue("@json", json);
-                cmd.ExecuteNonQuery();
-                con.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-            //json = json.Replace("-", "999");
-
-            JsonTextReader reader = new JsonTextReader(new StringReader(json));
-
-
+            //try
+            //{
+            //    ConfigData conf = new ConfigData();
+            //    conf = Config.GetConfig();
+            //    SqlConnection con = new SqlConnection(conf.DatabaseConn);
+            //    con.Open();
+            //    SqlCommand cmd = new SqlCommand("INSERT INTO json (jsonId, jsonString) values (1, @json)", con);
+            //    cmd.Parameters.AddWithValue("@json", json);
+            //    cmd.ExecuteNonQuery();
+            //    con.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Database error: " + ex.Message);
+            //}
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
+            
+            // read json string
+            JsonTextReader reader = new JsonTextReader(new StringReader(json));
+                        
+            // logging
+            List<string> log = new List<string>();
+
+            // Declare
+            TankData tank = new TankData();
+            string currentsection1 = "";
+            string currentsection3 = "";
+            string currentproperty = "";
+            object currentvalue = null;
+            
+            // Loop through json file
             while (reader.Read())
             {
-                String s;
-                if (reader.Value != null)
-                    s = reader.TokenType.ToString();
-                    //Console.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
-                else
-                    s = reader.TokenType.ToString();
-                    //Console.WriteLine("Token: {0}", reader.TokenType);
+                if (reader.Depth <= 1) // main level ( 0 or 1)
+                {
+                    if (reader.Value != null) // ********************************************  found main level - get section type  ************************************************************
+                    {
+                        string mainlevel = reader.Value.ToString();
+                        if (mainlevel == SectionType.header) currentsection1 = SectionType.header;
+                        if (mainlevel == SectionType.tanks) currentsection1 = SectionType.tanks;
+                        if (mainlevel == SectionType.tanks_v2) currentsection1 = SectionType.tanks_v2;
+                        log.Add("\nMain section: " + currentsection1 + "(Line: " + reader.LineNumber + ")");
+                    }
+                }
+
+                if (currentsection1 == SectionType.tanks || currentsection1 == SectionType.tanks_v2) // Only get data from tank or tank_v2 sections, skpi header for now....
+                {
+                    if (reader.Depth == 2) // ********************************************  found second level = tank level  ************************************************************
+                    {
+                        if (reader.Value != null) // found new tank
+                        {
+                            if (tank.tankName != "") // Tank data exist, save and log
+                            {
+                                log.Add("  Tank: '" + tank.tankName + "' | battleCount:" + tank.battleCount);
+                            }
+                            // Reset all values
+                            tank.Clear();
+                            // Get new tank name
+                            tank.tankName = reader.Value.ToString();
+                        }
+                    }
+                    else
+                    {
+                        if (reader.Depth == 3) // ********************************************  found third level = datatype  ************************************************************
+                        {
+                            if (reader.Value != null)
+                            {
+                                currentsection3 = reader.Value.ToString();
+                            }
+                        }
+                        else // ********************************************  found fourth level = property and value  ************************************************************
+                        {
+                            if (reader.TokenType == JsonToken.PropertyName)
+                            {
+                                // Property
+                                currentproperty = reader.Value.ToString();
+                            }
+                            else
+                            {
+                                if (reader.Value != null)
+                                {
+                                    // Value
+                                    currentvalue = reader.Value.ToString();
+
+                                    // Check data here - make separate class for this later
+                                    //
+                                    if (currentsection1 == SectionType.tanks)
+                                    {
+                                        if (currentsection3 == "tankdata" && currentproperty == "battlesCount") tank.battleCount = Convert.ToInt32(currentvalue);
+                                    }
+                                    else if (currentsection1 == SectionType.tanks_v2)
+                                    {
+                                        if (currentsection3 == "a15x15" && currentproperty == "battlesCount") tank.battleCount = Convert.ToInt32(currentvalue);
+                                    }
+
+                                    // Temp log all data
+                                    // log.Add("      > " + currentsection1 + "  |  " + tank.tankName + "  |  " + currentsection3 + "  |  " + currentproperty + ":" + currentvalue.ToString());
+                                }
+                            }
+                        }
+                    }
+                }
             }
+            reader.Close();
+            
             sw.Stop();
             TimeSpan ts = sw.Elapsed;
-
-            //// Config data 
-            //ConfigData conf = new ConfigData();
-            //conf = Config.GetConfig();
-            //// Connect to DB 
-            //SqlConnection myConnection = new SqlConnection(conf.DatabaseConn);
-            //SqlCommand myCommand = new SqlCommand();
-            //SqlDataReader dr;
-            //// Get all tanks 
-            //string sql = "SELECT * FROM tanks";
-            //myConnection.Open();
-            //myCommand = new SqlCommand(sql, myConnection);
-            //dr = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
-
-            //// Loop through datareader 
-            //Stopwatch s = new Stopwatch();
-            //s.Start();
-            //while (dr.Read())
-            //{
-            //    string tankTitle = dr["tankTitle"].ToString();
-            //    JObject dossier = JObject.Parse(json);
-            //    int battleCount;
-            //    try
-            //    {
-            //        battleCount = (int)dossier["tanks_v2"][tankTitle]["a15x15"]["battlesCount"];
-            //    }
-            //    catch (Exception)
-            //    {
-                    
-            //    }
-            //    try
-            //    {
-            //        battleCount = (int)dossier["tanks"][tankTitle]["tankdata"]["battlesCount"];
-            //    }
-            //    catch (Exception)
-            //    {
-            //        battleCount = 0;
-            //    }
-            //    //Console.WriteLine(tankTitle + " " + battleCount);
-            //}
-            //s.Stop();
-            //TimeSpan ts = s.Elapsed;
-            
-            ////Console.WriteLine(ts.Minutes + ":" + ts.Seconds + ":" + ts.Milliseconds);
-            //myConnection.Close();
+            Log.LogToFile(log);
 
 
-            return (" > Time spent analyzing file: " + ts.Minutes + ":" + ts.Seconds + ":" + ts.Milliseconds);
+            return (" > Time spent analyzing file: " + ts.Minutes + ":" + ts.Seconds + ":" + ts.Milliseconds.ToString("000"));
         }
     }
 }
