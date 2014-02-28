45b8e24e97f20ca896ea9eeb4c438450ae9e53df
leerude <sitarhero13@gmail.com>
2014-02-28 01:25:19 +0100

    Completed importTurrets()

--- WotDBUpdater/Business.Logic/mudules2DB.cs
+++ WotDBUpdater/Business.Logic/mudules2DB.cs
@@ -21,7 +21,9 @@ namespace WotDBUpdater
 
         public static String fetchFromAPI()
         {
-            string url = "https://api.worldoftanks.eu/wot/encyclopedia/tankturrets/?application_id=2a8bf9a1ee36d6125058bf6efd006caf";
+            Log.LogToFile("test");
+
+            string url = "https://api.worldoftanks.eu/wot/encyclopedia/tankturrets/?application_id=0a7f2eb79dce0dd45df9b8fedfed7530";
             HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(url);
 
             httpRequest.Timeout = 10000;     // 10 secs
@@ -45,6 +47,7 @@ namespace WotDBUpdater
             int moduleCount;
             JToken rootToken;
             JToken moduleToken;
+            string sql = "";
 
             JObject allTokens = JObject.Parse(json);
             rootToken = allTokens.First;
@@ -56,48 +59,51 @@ namespace WotDBUpdater
 
                 rootToken = rootToken.Next;  // start reading modules
 
-                JToken turrets = rootToken.Children().First();
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
+                JToken turrets = rootToken.Children().First();  // read all tokens 
+                
                 foreach (JProperty turret in turrets)
                 { 
                     moduleToken = turret.First();
-                    string parent = ((JProperty)moduleToken.Parent).Name.ToString();
-                    string nation_i18n = moduleToken["nation_i18n"].ToString();
-                    string armor_fedd = moduleToken["armor_fedd"].ToString();
-                    string circular_vision_radius = moduleToken["circular_vision_radius"].ToString();
+                    
+                    int turretId = Int32.Parse(((JProperty)moduleToken.Parent).Name);
+                    JArray tanksArray = (JArray)moduleToken["tanks"];
+                    int tankId = Int32.Parse(tanksArray[0].ToString());
+                    string name = moduleToken["name_i18n"].ToString();
+                    int tier = Int32.Parse(moduleToken["level"].ToString());
+                    int viewRange = Int32.Parse(moduleToken["circular_vision_radius"].ToString());
+                    int armorFront = Int32.Parse(moduleToken["armor_forehead"].ToString());
+                    int armorSides = Int32.Parse(moduleToken["armor_board"].ToString());
+                    int armorRear = Int32.Parse(moduleToken["armor_fedd"].ToString());
+                    
                     //string weight = moduleToken["weight"].ToString();
                     //string s = moduleToken["name"].ToString();
-                    JArray tanksArray = (JArray)moduleToken["tanks"];
-                    string tanks = tanksArray[0].ToString();
+
+                    sql = sql + "insert into turret (turretId, tankId, name, tier, viewRange, armorFront, armorSides, armorRear) values"
+                              + "('" + turretId + "', '" + tankId + "', '" + name + "', '" + tier + "', '" + viewRange + "', '" + armorFront 
+                              + "', '" + armorSides + "', '" + armorRear +"'); ";
                 }
-                sw.Stop();
-                TimeSpan ts = sw.Elapsed;
+                write2DB(sql);
             }
 
             return ("Import Complete");
         }
 
 
-        //public static void write2DB()
-        //{
-        //    try
-        //    {
-        //        // Cet config data
-        //        SqlConnection con = new SqlConnection(Config.Settings.DatabaseConn);
-        //        con.Open();
-        //        SqlCommand cmd = new SqlCommand("INSERT INTO country (countryid, name, shortname) VALUES (@countryid, @name, @shortname)", con);
-        //        cmd.Parameters.AddWithValue("@countryid", txtid.Text);
-        //        cmd.Parameters.AddWithValue("@name", txtName.Text);
-        //        cmd.Parameters.AddWithValue("@shortname", txtShortName.Text);
-        //        cmd.ExecuteNonQuery();
-        //        con.Close();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //    }
-        //}
+        public static void write2DB(string sql)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(Config.Settings.DatabaseConn);
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
 
         #endregion
