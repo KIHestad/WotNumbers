596401576c1ed38c52648dd5393b0091e0217978
leerude <sitarhero13@gmail.com>
2014-03-01 14:20:51 +0100

    dbscripts cleanup

---
+++ WotDBUpdater/Business.Logic/ratings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WotDBUpdater.Business.Logic
+{
+    class ratings
+    {
+        public static void efficiancy()
+        {
+            //SqlConnection con = new SqlConnection(Config.Settings.DatabaseConn);
+            //SqlCommand cmd = new SqlCommand("SELECT * FROM country", con);
+            //cmd.CommandType = CommandType.Text;
+            //SqlDataAdapter sda = new SqlDataAdapter(cmd);
+
+
+        }
+    }
+}
