2b6b7c5443739617bafde2a2e72e38004eeb640b
BadButton <kih@hestad.no>
2014-03-17 05:18:32 +0100

    local commit from source tree

---
+++ WotDBUpdater/Code/Support/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WotDBUpdater.Forms;
+
+namespace WotDBUpdater.Code.Support
+{
+    class Message
+    {
+        public static void Show(string title, string message)
+        {
+            Form frm = new Forms.Message(title, message);
+            frm.FormBorderStyle = FormBorderStyle.None;
+            frm.ShowDialog();
+        }
+    }
+}
