f905e73e479328b4fa492e6dcfce40edd5a0c92e
BadButton <kih@hestad.no>
2014-03-30 15:11:00 +0200

    small fix, moved SQLite interop files

--- WotDBUpdater/Forms/File/DatabaseNew.Designer.cs
+++ WotDBUpdater/Forms/File/DatabaseNew.Designer.cs
@@ -28,122 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSave = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtPlayerName = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtFileLocation = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtDatabasename = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pbCreateDatabase = new System.Windows.Forms.ProgressBar();
-            this.groupBox1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(319, 154);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(106, 25);
-            this.btnSave.TabIndex = 11;
-            this.btnSave.Text = "Create database";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(211, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Player name:";
-            // 
-            // txtPlayerName
-            // 
-            this.txtPlayerName.Location = new System.Drawing.Point(214, 43);
-            this.txtPlayerName.Name = "txtPlayerName";
-            this.txtPlayerName.Size = new System.Drawing.Size(181, 20);
-            this.txtPlayerName.TabIndex = 9;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.txtFileLocation);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.txtDatabasename);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.txtPlayerName);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(413, 126);
-            this.groupBox1.TabIndex = 12;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Settings";
-            // 
-            // txtFileLocation
-            // 
-            this.txtFileLocation.Location = new System.Drawing.Point(15, 88);
-            this.txtFileLocation.Name = "txtFileLocation";
-            this.txtFileLocation.Size = new System.Drawing.Size(380, 20);
-            this.txtFileLocation.TabIndex = 13;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 72);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 13);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "Database file location:";
-            // 
-            // txtDatabasename
-            // 
-            this.txtDatabasename.Location = new System.Drawing.Point(15, 43);
-            this.txtDatabasename.Name = "txtDatabasename";
-            this.txtDatabasename.Size = new System.Drawing.Size(181, 20);
-            this.txtDatabasename.TabIndex = 11;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 27);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 13);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Database name:";
-            // 
-            // pbCreateDatabase
-            // 
-            this.pbCreateDatabase.Location = new System.Drawing.Point(12, 155);
-            this.pbCreateDatabase.MarqueeAnimationSpeed = 1;
-            this.pbCreateDatabase.Name = "pbCreateDatabase";
-            this.pbCreateDatabase.Size = new System.Drawing.Size(301, 23);
-            this.pbCreateDatabase.Step = 1;
-            this.pbCreateDatabase.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.pbCreateDatabase.TabIndex = 13;
-            this.pbCreateDatabase.UseWaitCursor = true;
-            this.pbCreateDatabase.Value = 1;
-            this.pbCreateDatabase.Visible = false;
-            // 
-            // frmDatabaseNew
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 193);
-            this.Controls.Add(this.pbCreateDatabase);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.btnSave);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "frmDatabaseNew";
-            this.ShowInTaskbar = false;
-            this.Text = "Create New Database";
-            this.Load += new System.EventHandler(this.frmDatabaseNew_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.ResumeLayout(false);
+			this.btnSave = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtPlayerName = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.txtFileLocation = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtDatabasename = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pbCreateDatabase = new System.Windows.Forms.ProgressBar();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(319, 154);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(106, 25);
+			this.btnSave.TabIndex = 11;
+			this.btnSave.Text = "Create database";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(211, 27);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 13);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Player name:";
+			// 
+			// txtPlayerName
+			// 
+			this.txtPlayerName.Location = new System.Drawing.Point(214, 43);
+			this.txtPlayerName.Name = "txtPlayerName";
+			this.txtPlayerName.Size = new System.Drawing.Size(181, 20);
+			this.txtPlayerName.TabIndex = 9;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.txtFileLocation);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.txtDatabasename);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.txtPlayerName);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(413, 126);
+			this.groupBox1.TabIndex = 12;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Settings";
+			// 
+			// txtFileLocation
+			// 
+			this.txtFileLocation.Location = new System.Drawing.Point(15, 88);
+			this.txtFileLocation.Name = "txtFileLocation";
+			this.txtFileLocation.Size = new System.Drawing.Size(380, 20);
+			this.txtFileLocation.TabIndex = 13;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 72);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(112, 13);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "Database file location:";
+			// 
+			// txtDatabasename
+			// 
+			this.txtDatabasename.Location = new System.Drawing.Point(15, 43);
+			this.txtDatabasename.Name = "txtDatabasename";
+			this.txtDatabasename.Size = new System.Drawing.Size(181, 20);
+			this.txtDatabasename.TabIndex = 11;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 27);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(85, 13);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "Database name:";
+			// 
+			// pbCreateDatabase
+			// 
+			this.pbCreateDatabase.Location = new System.Drawing.Point(12, 155);
+			this.pbCreateDatabase.MarqueeAnimationSpeed = 1;
+			this.pbCreateDatabase.Name = "pbCreateDatabase";
+			this.pbCreateDatabase.Size = new System.Drawing.Size(301, 23);
+			this.pbCreateDatabase.Step = 1;
+			this.pbCreateDatabase.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+			this.pbCreateDatabase.TabIndex = 13;
+			this.pbCreateDatabase.UseWaitCursor = true;
+			this.pbCreateDatabase.Value = 1;
+			this.pbCreateDatabase.Visible = false;
+			// 
+			// DatabaseNew
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(443, 193);
+			this.Controls.Add(this.pbCreateDatabase);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.btnSave);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "DatabaseNew";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Create New Database";
+			this.Load += new System.EventHandler(this.frmDatabaseNew_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
 
         }
 
