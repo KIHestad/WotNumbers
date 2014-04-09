3677ae7278a95e738cfa72065f022203ed1bf429
BadButton <kih@hestad.no>
2014-04-10 01:32:59 +0200

    Removed Stored procedures used for WotStat Import

--- WotDBUpdater/Forms/File/DatabaseNew.cs
+++ WotDBUpdater/Forms/File/DatabaseNew.cs
@@ -35,13 +35,13 @@ namespace WotDBUpdater.Forms.File
 			Application.DoEvents();
 		}
 
-        private void UpdateSQLiteProgressBar(ref int step, int maxStep)
-        {
-            step++; // count step 1 to maxValue
-            int maxValue = (int)((double)maxStep / ((double)step / (double)maxStep) * 100); // calculate the maxValue to be correct according to that value = 1 all time
-            pbCreateSQLiteDB.Maximum = maxValue;
-            Application.DoEvents();
-        }
+		private void UpdateSQLiteProgressBar(ref int step, int maxStep)
+		{
+			step++; // count step 1 to maxValue
+			int maxValue = (int)((double)maxStep / ((double)step / (double)maxStep) * 100); // calculate the maxValue to be correct according to that value = 1 all time
+			pbCreateSQLiteDB.Maximum = maxValue;
+			Application.DoEvents();
+		}
 
 
 		private bool CreateDatabase(string databaseName, string fileLocation)
@@ -129,17 +129,17 @@ namespace WotDBUpdater.Forms.File
 		}
 
 
-        private void RunSQLite(string sqlbatch)
-        {
-            /*
-            string sql = File.ReadAllText("x.sql");
-            string db = "Data Source=C:\\dev\\wotdb\\WotDBUpdater\\bin\\Debug\\wotdb.db";
-            string dbConnection = db;
-            SQLiteConnection conn = new SQLiteConnection(dbConnection);
-            conn.Open();
-            SQLiteCommand mycommand = new SQLiteCommand(sql, conn);
-             */
-        }
+		private void RunSQLite(string sqlbatch)
+		{
+			/*
+			string sql = File.ReadAllText("x.sql");
+			string db = "Data Source=C:\\dev\\wotdb\\WotDBUpdater\\bin\\Debug\\wotdb.db";
+			string dbConnection = db;
+			SQLiteConnection conn = new SQLiteConnection(dbConnection);
+			conn.Open();
+			SQLiteCommand mycommand = new SQLiteCommand(sql, conn);
+			 */
+		}
 
 
 
@@ -163,7 +163,7 @@ namespace WotDBUpdater.Forms.File
 			else
 			{
 				Cursor.Current = Cursors.WaitCursor;
-				int maxStep = 14;
+				int maxStep = 13;
 				pbCreateDatabase.Maximum = maxStep * 100;
 				pbCreateDatabase.Value = maxStep * 100;
 				pbCreateDatabase.Visible = true;
@@ -186,6 +186,106 @@ namespace WotDBUpdater.Forms.File
 					StreamReader streamReader = new StreamReader(path + "createTable.txt", Encoding.UTF8);
 					sql = streamReader.ReadToEnd();
 					RunSql(sql);
+					UpdateProgressBar(ref step, maxStep);
+					// Create Views
+					streamReader = new StreamReader(path + "createView.txt", Encoding.UTF8);
+					sql = streamReader.ReadToEnd();
+					RunSql(sql);
+					UpdateProgressBar(ref step, maxStep);
+					// Insert default data
+					streamReader = new StreamReader(path + "insert.txt", Encoding.UTF8);
+					sql = streamReader.ReadToEnd();
+					RunSql(sql);
+					UpdateProgressBar(ref step, maxStep);
+					// Get tanks, remember to init tankList first
+					TankData.GetTankListFromDB();
+					Application.DoEvents();
+					ImportMisc2DB.UpdateTanks();
+					Application.DoEvents();
+					// Init after getting tanks and other basic data import
+					TankData.GetTankListFromDB();
+					TankData.GetJson2dbMappingViewFromDB();
+					TankData.GettankData2BattleMappingViewFromDB();
+					UpdateProgressBar(ref step, maxStep);
+					// Get turret
+					ImportWotApi2DB.ImportTurrets();
+					UpdateProgressBar(ref step, maxStep);
+					// Get guns
+					ImportWotApi2DB.ImportGuns();
+					UpdateProgressBar(ref step, maxStep);
+					// Get radios
+					ImportWotApi2DB.ImportRadios();
+					UpdateProgressBar(ref step, maxStep);
+					// Get achievements
+					ImportWotApi2DB.ImportAchievements();
+					UpdateProgressBar(ref step, maxStep);
+					// Get WN8 ratings
+					ImportMisc2DB.UpdateWN8();
+					UpdateProgressBar(ref step, maxStep);
+					// Add player
+					if (txtPlayerName.Text.Trim() != "")
+					{
+						RunSql("INSERT INTO player (name) VALUES ('" + txtPlayerName.Text.Trim() + "')");
+						Config.Settings.playerName = txtPlayerName.Text.Trim();
+						Config.SaveAppConfig(out result);
+						UpdateProgressBar(ref step, maxStep);
+					}
+					UpdateProgressBar(ref step, maxStep);
+					// Done creating db, save settings
+					string msg = "";
+					Config.SaveDbConfig(out msg);
+					Config.SaveAppConfig(out msg);
+					UpdateProgressBar(ref step, maxStep);
+					// Done
+					Cursor.Current = Cursors.Default;
+					Application.DoEvents();
+					Code.Support.MessageDark.Show("Database created successfully.", "Created database");
+					pbCreateDatabase.Visible = false;
+					Form.ActiveForm.Close();
+				}
+			}
+		}
+
+
+
+
+		private void btnCreateSQLiteDB_Click(object sender, EventArgs e)
+		{
+			// Check if database exists
+			bool dbExists = false;
+			string db = "C:\\dev\\wotdb\\WotDBUpdater\\bin\\Debug\\wotdb.db";
+			SQLiteConnection.CreateFile(db);
+			//string dbConnection = db;
+			//SQLiteConnection cnn = new SQLiteConnection(dbConnection);
+			//cnn.Open();
+			//SQLiteCommand mycommand = new SQLiteCommand(cnn);
+
+
+			/*
+			{
+				Cursor.Current = Cursors.WaitCursor;
+				int maxStep = 14;
+				pbCreateSQLiteDB.Maximum = maxStep * 100;
+				pbCreateSQLiteDB.Value = maxStep * 100;
+				pbCreateSQLiteDB.Visible = true;
+				int step = 0;
+				UpdateSQLiteProgressBar(ref step, maxStep);
+				Application.DoEvents();
+				
+				{
+					// Save new database to Config
+					Config.Settings.databaseName = "wotdb.db";
+					string result = "";
+					//Config.SaveDbConfig(out result);
+					// Fill database with default data
+					UpdateProgressBar(ref step, maxStep);
+					// Update db by running sql scripts
+					string path = Path.GetDirectoryName(Application.ExecutablePath) + "\\Docs\\Database\\";
+					string sql;
+					// Create Tables
+					StreamReader streamReader = new StreamReader(path + "createTable.txt", Encoding.UTF8);
+					sql = streamReader.ReadToEnd();
+					RunSQLite(sql);
 					UpdateProgressBar(ref step, maxStep);
 					// Create Views
 					streamReader = new StreamReader(path + "createView.txt", Encoding.UTF8);
@@ -249,119 +349,14 @@ namespace WotDBUpdater.Forms.File
 					Form.ActiveForm.Close();
 				}
 			}
+			 */
 		}
 
 
+		private void badForm1_Click(object sender, EventArgs e)
+		{
 
-
-        private void btnCreateSQLiteDB_Click(object sender, EventArgs e)
-        {
-            // Check if database exists
-            bool dbExists = false;
-            string db = "C:\\dev\\wotdb\\WotDBUpdater\\bin\\Debug\\wotdb.db";
-            SQLiteConnection.CreateFile(db);
-            //string dbConnection = db;
-            //SQLiteConnection cnn = new SQLiteConnection(dbConnection);
-            //cnn.Open();
-            //SQLiteCommand mycommand = new SQLiteCommand(cnn);
-
-
-            /*
-            {
-                Cursor.Current = Cursors.WaitCursor;
-                int maxStep = 14;
-                pbCreateSQLiteDB.Maximum = maxStep * 100;
-                pbCreateSQLiteDB.Value = maxStep * 100;
-                pbCreateSQLiteDB.Visible = true;
-                int step = 0;
-                UpdateSQLiteProgressBar(ref step, maxStep);
-                Application.DoEvents();
-                
-                {
-                    // Save new database to Config
-                    Config.Settings.databaseName = "wotdb.db";
-                    string result = "";
-                    //Config.SaveDbConfig(out result);
-                    // Fill database with default data
-                    UpdateProgressBar(ref step, maxStep);
-                    // Update db by running sql scripts
-                    string path = Path.GetDirectoryName(Application.ExecutablePath) + "\\Docs\\Database\\";
-                    string sql;
-                    // Create Tables
-                    StreamReader streamReader = new StreamReader(path + "createTable.txt", Encoding.UTF8);
-                    sql = streamReader.ReadToEnd();
-                    RunSQLite(sql);
-                    UpdateProgressBar(ref step, maxStep);
-                    // Create Views
-                    streamReader = new StreamReader(path + "createView.txt", Encoding.UTF8);
-                    sql = streamReader.ReadToEnd();
-                    RunSql(sql);
-                    UpdateProgressBar(ref step, maxStep);
-                    // Create Stored Procedures
-                    streamReader = new StreamReader(path + "createProc.txt", Encoding.UTF8);
-                    sql = streamReader.ReadToEnd();
-                    RunSql(sql);
-                    UpdateProgressBar(ref step, maxStep);
-                    // Insert default data
-                    streamReader = new StreamReader(path + "insert.txt", Encoding.UTF8);
-                    sql = streamReader.ReadToEnd();
-                    RunSql(sql);
-                    UpdateProgressBar(ref step, maxStep);
-                    // Get tanks, remember to init tankList first
-                    TankData.GetTankListFromDB();
-                    Application.DoEvents();
-                    ImportMisc2DB.UpdateTanks();
-                    Application.DoEvents();
-                    // Init after getting tanks and other basic data import
-                    TankData.GetTankListFromDB();
-                    TankData.GetJson2dbMappingViewFromDB();
-                    TankData.GettankData2BattleMappingViewFromDB();
-                    UpdateProgressBar(ref step, maxStep);
-                    // Get turret
-                    ImportWotApi2DB.ImportTurrets();
-                    UpdateProgressBar(ref step, maxStep);
-                    // Get guns
-                    ImportWotApi2DB.ImportGuns();
-                    UpdateProgressBar(ref step, maxStep);
-                    // Get radios
-                    ImportWotApi2DB.ImportRadios();
-                    UpdateProgressBar(ref step, maxStep);
-                    // Get achievements
-                    ImportWotApi2DB.ImportAchievements();
-                    UpdateProgressBar(ref step, maxStep);
-                    // Get WN8 ratings
-                    ImportMisc2DB.UpdateWN8();
-                    UpdateProgressBar(ref step, maxStep);
-                    // Add player
-                    if (txtPlayerName.Text.Trim() != "")
-                    {
-                        RunSql("INSERT INTO player (name) VALUES ('" + txtPlayerName.Text.Trim() + "')");
-                        Config.Settings.playerName = txtPlayerName.Text.Trim();
-                        Config.SaveAppConfig(out result);
-                        UpdateProgressBar(ref step, maxStep);
-                    }
-                    UpdateProgressBar(ref step, maxStep);
-                    // Done creating db, save settings
-                    string msg = "";
-                    Config.SaveDbConfig(out msg);
-                    Config.SaveAppConfig(out msg);
-                    UpdateProgressBar(ref step, maxStep);
-                    // Done
-                    Cursor.Current = Cursors.Default;
-                    Application.DoEvents();
-                    Code.Support.MessageDark.Show("Database created successfully.", "Created database");
-                    pbCreateDatabase.Visible = false;
-                    Form.ActiveForm.Close();
-                }
-            }
-             */
-        }
-
-
-        private void badForm1_Click(object sender, EventArgs e)
-        {
-
-        }
+		}
 
 
 	}
