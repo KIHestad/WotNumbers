ead154bf97a7b84c9b62cef6f7ad1340ad110e65
BadButton <kih@hestad.no>
2014-07-11 02:49:03 +0200

    assembly info

--- WinApp/Properties/AssemblyInfo.cs
+++ WinApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WotDBUpdater")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Wot Numbers")]
+[assembly: AssemblyDescription("World of Tanks progress and performance tracker")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WotDBUpdater")]
+[assembly: AssemblyCompany("Wot Numbers Team")]
+[assembly: AssemblyProduct("Wot Numbers")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
