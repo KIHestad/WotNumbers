034497146a7fc2fea86e9050af25eae549984cbf
BadButton <kih@hestad.no>
2014-07-15 22:04:07 +0200

    Battle Chart Form

---
+++ WinApp/Forms/BattleChart.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WinApp.Forms
+{
+	partial class BattleChart
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			BadThemeContainerControl.MainAreaClass mainAreaClass1 = new BadThemeContainerControl.MainAreaClass();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BattleChart));
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			this.BattleChartTheme = new BadForm();
+			this.btnClearChart = new BadButton();
+			this.ddXaxis = new BadDropDownBox();
+			this.badLabel3 = new BadLabel();
+			this.ddValue = new BadDropDownBox();
+			this.badLabel2 = new BadLabel();
+			this.btnAddChart = new BadButton();
+			this.ddTank = new BadDropDownBox();
+			this.badLabel1 = new BadLabel();
+			this.ChartingMain = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.badGroupBox1 = new BadGroupBox();
+			this.BattleChartTheme.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ChartingMain)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// BattleChartTheme
+			// 
+			this.BattleChartTheme.BackColor = System.Drawing.Color.Fuchsia;
+			this.BattleChartTheme.Controls.Add(this.btnClearChart);
+			this.BattleChartTheme.Controls.Add(this.ddXaxis);
+			this.BattleChartTheme.Controls.Add(this.badLabel3);
+			this.BattleChartTheme.Controls.Add(this.ddValue);
+			this.BattleChartTheme.Controls.Add(this.badLabel2);
+			this.BattleChartTheme.Controls.Add(this.btnAddChart);
+			this.BattleChartTheme.Controls.Add(this.ddTank);
+			this.BattleChartTheme.Controls.Add(this.badLabel1);
+			this.BattleChartTheme.Controls.Add(this.ChartingMain);
+			this.BattleChartTheme.Controls.Add(this.badGroupBox1);
+			this.BattleChartTheme.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.BattleChartTheme.FormBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.BattleChartTheme.FormFooter = false;
+			this.BattleChartTheme.FormFooterHeight = 26;
+			this.BattleChartTheme.FormInnerBorder = 3;
+			this.BattleChartTheme.FormMargin = 0;
+			this.BattleChartTheme.Image = null;
+			this.BattleChartTheme.Location = new System.Drawing.Point(0, 0);
+			this.BattleChartTheme.MainArea = mainAreaClass1;
+			this.BattleChartTheme.Name = "BattleChartTheme";
+			this.BattleChartTheme.Resizable = true;
+			this.BattleChartTheme.Size = new System.Drawing.Size(755, 428);
+			this.BattleChartTheme.SystemExitImage = ((System.Drawing.Image)(resources.GetObject("BattleChartTheme.SystemExitImage")));
+			this.BattleChartTheme.SystemMaximizeImage = null;
+			this.BattleChartTheme.SystemMinimizeImage = null;
+			this.BattleChartTheme.TabIndex = 0;
+			this.BattleChartTheme.Text = "Battle Chart";
+			this.BattleChartTheme.TitleHeight = 26;
+			// 
+			// btnClearChart
+			// 
+			this.btnClearChart.Image = null;
+			this.btnClearChart.Location = new System.Drawing.Point(660, 65);
+			this.btnClearChart.Name = "btnClearChart";
+			this.btnClearChart.Size = new System.Drawing.Size(56, 23);
+			this.btnClearChart.TabIndex = 14;
+			this.btnClearChart.Text = "Clear";
+			this.btnClearChart.Click += new System.EventHandler(this.btnClearChart_Click);
+			// 
+			// ddXaxis
+			// 
+			this.ddXaxis.Image = null;
+			this.ddXaxis.Location = new System.Drawing.Point(497, 65);
+			this.ddXaxis.Name = "ddXaxis";
+			this.ddXaxis.Size = new System.Drawing.Size(80, 23);
+			this.ddXaxis.TabIndex = 13;
+			this.ddXaxis.Text = "Date";
+			this.ddXaxis.TextChanged += new System.EventHandler(this.ddXaxis_TextChanged);
+			this.ddXaxis.Click += new System.EventHandler(this.ddXaxis_Click);
+			// 
+			// badLabel3
+			// 
+			this.badLabel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+			this.badLabel3.Dimmed = false;
+			this.badLabel3.FontColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(206)))));
+			this.badLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(206)))));
+			this.badLabel3.Image = null;
+			this.badLabel3.Location = new System.Drawing.Point(454, 65);
+			this.badLabel3.Name = "badLabel3";
+			this.badLabel3.Size = new System.Drawing.Size(47, 23);
+			this.badLabel3.TabIndex = 12;
+			this.badLabel3.Text = "X-Axis:";
+			// 
+			// ddValue
+			// 
+			this.ddValue.Image = null;
+			this.ddValue.Location = new System.Drawing.Point(280, 65);
+			this.ddValue.Name = "ddValue";
+			this.ddValue.Size = new System.Drawing.Size(160, 23);
+			this.ddValue.TabIndex = 11;
+			this.ddValue.Click += new System.EventHandler(this.ddValue_Click);
+			// 
+			// badLabel2
+			// 
+			this.badLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+			this.badLabel2.Dimmed = false;
+			this.badLabel2.FontColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(206)))));
+			this.badLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(206)))));
+			this.badLabel2.Image = null;
+			this.badLabel2.Location = new System.Drawing.Point(240, 65);
+			this.badLabel2.Name = "badLabel2";
+			this.badLabel2.Size = new System.Drawing.Size(49, 23);
+			this.badLabel2.TabIndex = 10;
+			this.badLabel2.Text = "Value:";
+			// 
+			// btnAddChart
+			// 
+			this.btnAddChart.Image = null;
+			this.btnAddChart.Location = new System.Drawing.Point(598, 65);
+			this.btnAddChart.Name = "btnAddChart";
+			this.btnAddChart.Size = new System.Drawing.Size(56, 23);
+			this.btnAddChart.TabIndex = 8;
+			this.btnAddChart.Text = "Add";
+			this.btnAddChart.Click += new System.EventHandler(this.btnAddChart_Click);
+			// 
+			// ddTank
+			// 
+			this.ddTank.Image = null;
+			this.ddTank.Location = new System.Drawing.Point(69, 65);
+			this.ddTank.Name = "ddTank";
+			this.ddTank.Size = new System.Drawing.Size(160, 23);
+			this.ddTank.TabIndex = 3;
+			this.ddTank.Click += new System.EventHandler(this.ddTank_Click);
+			// 
+			// badLabel1
+			// 
+			this.badLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+			this.badLabel1.Dimmed = false;
+			this.badLabel1.FontColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(206)))));
+			this.badLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(206)))));
+			this.badLabel1.Image = null;
+			this.badLabel1.Location = new System.Drawing.Point(32, 65);
+			this.badLabel1.Name = "badLabel1";
+			this.badLabel1.Size = new System.Drawing.Size(41, 23);
+			this.badLabel1.TabIndex = 7;
+			this.badLabel1.Text = "Tank:";
+			// 
+			// ChartingMain
+			// 
+			this.ChartingMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.ChartingMain.BackColor = System.Drawing.Color.Transparent;
+			chartArea1.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Days;
+			chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.Silver;
+			chartArea1.AxisX.LineColor = System.Drawing.Color.Silver;
+			chartArea1.AxisX.ScaleBreakStyle.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisX.TitleForeColor = System.Drawing.Color.Silver;
+			chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.Silver;
+			chartArea1.AxisY.LineColor = System.Drawing.Color.Silver;
+			chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY.MajorTickMark.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY.MinorGrid.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY.MinorTickMark.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY.ScaleBreakStyle.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY.TitleForeColor = System.Drawing.Color.Silver;
+			chartArea1.AxisY2.LabelStyle.ForeColor = System.Drawing.Color.Silver;
+			chartArea1.AxisY2.MajorGrid.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY2.MajorTickMark.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY2.MinorGrid.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY2.MinorTickMark.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY2.ScaleBreakStyle.LineColor = System.Drawing.Color.DimGray;
+			chartArea1.AxisY2.ScaleBreakStyle.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+			chartArea1.AxisY2.TitleForeColor = System.Drawing.Color.Silver;
+			chartArea1.BackColor = System.Drawing.Color.Transparent;
+			chartArea1.BackSecondaryColor = System.Drawing.Color.Transparent;
+			chartArea1.BorderColor = System.Drawing.Color.White;
+			chartArea1.Name = "ChartArea1";
+			chartArea1.ShadowColor = System.Drawing.Color.Transparent;
+			this.ChartingMain.ChartAreas.Add(chartArea1);
+			legend1.BackColor = System.Drawing.Color.Transparent;
+			legend1.ForeColor = System.Drawing.Color.Silver;
+			legend1.Name = "Legend1";
+			this.ChartingMain.Legends.Add(legend1);
+			this.ChartingMain.Location = new System.Drawing.Point(9, 108);
+			this.ChartingMain.Name = "ChartingMain";
+			this.ChartingMain.Size = new System.Drawing.Size(730, 305);
+			this.ChartingMain.TabIndex = 0;
+			this.ChartingMain.Text = "chart1";
+			// 
+			// badGroupBox1
+			// 
+			this.badGroupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.badGroupBox1.BackColor = System.Drawing.Color.Transparent;
+			this.badGroupBox1.Image = null;
+			this.badGroupBox1.Location = new System.Drawing.Point(17, 40);
+			this.badGroupBox1.Name = "badGroupBox1";
+			this.badGroupBox1.Size = new System.Drawing.Size(713, 62);
+			this.badGroupBox1.TabIndex = 9;
+			this.badGroupBox1.Text = "Add Chart line";
+			// 
+			// BattleChart
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Fuchsia;
+			this.ClientSize = new System.Drawing.Size(755, 428);
+			this.Controls.Add(this.BattleChartTheme);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.MinimumSize = new System.Drawing.Size(400, 300);
+			this.Name = "BattleChart";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "TestShowImage";
+			this.TransparencyKey = System.Drawing.Color.Fuchsia;
+			this.Load += new System.EventHandler(this.BattleChart_Load);
+			this.BattleChartTheme.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.ChartingMain)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private BadForm BattleChartTheme;
+		private System.Windows.Forms.DataVisualization.Charting.Chart ChartingMain;
+		private BadDropDownBox ddTank;
+		private BadLabel badLabel1;
+		private BadDropDownBox ddXaxis;
+		private BadLabel badLabel3;
+		private BadDropDownBox ddValue;
+		private BadLabel badLabel2;
+		private BadButton btnAddChart;
+		private BadGroupBox badGroupBox1;
+		private BadButton btnClearChart;
+
+
+
+	}
+}
---
+++ WinApp/Forms/BattleChart.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using WinApp.Code;
+
+namespace WinApp.Forms
+{
+	public partial class BattleChart : Form
+	{
+		string ddTankList = "";
+		string ddChartList = "";
+		int initPlayerTankId = 0;
+
+		public BattleChart(int playerTankId = 0)
+		{
+			InitializeComponent();
+			initPlayerTankId = playerTankId;
+		}
+
+		private class ChartValue
+		{
+			public string name = "";	// Name of Chart VAlue in dropdown 
+			public string ptbCol = "";	// playerTankBattle column to get current value 
+			public string bCol = "";	// battle column to get battle value
+		}
+
+		private List<ChartValue> chartValues = new List<ChartValue>();
+
+		private void AddChartValues()
+		{
+			chartValues.Add(new ChartValue()
+			{
+				name = "Win Rate",
+				ptbCol = "",
+				bCol = ""
+			});
+			
+			chartValues.Add(new ChartValue()
+			{
+				name = "EFF",
+				ptbCol = "battles",
+				bCol = "battlesCount"
+			});
+
+			chartValues.Add(new ChartValue()
+			{
+				name = "WN8",
+				ptbCol = "battles",
+				bCol = "battlesCount"
+			});
+
+			chartValues.Add(new ChartValue()
+			{
+				name = "Total XP",
+				ptbCol = "xp",
+				bCol = "xp"
+			});
+
+			chartValues.Add(new ChartValue() 
+			{ 
+				name = "Battle Count", 
+				ptbCol = "battles", 
+				bCol= "battlesCount" 
+			});
+
+			chartValues.Add(new ChartValue()
+			{
+				name = "Victory Count",
+				ptbCol = "wins",
+				bCol = "victory"
+			});
+
+			chartValues.Add(new ChartValue()
+			{
+				name = "Draw Count",
+				ptbCol = "(battles - wins - losses)",
+				bCol = "draw"
+			});
+
+			chartValues.Add(new ChartValue()
+			{
+				name = "Defeat Count",
+				ptbCol = "losses",
+				bCol = "defeat"
+			});
+
+
+		}
+
+		private void BattleChart_Load(object sender, EventArgs e)
+		{
+			// Init showing tank or all tanks
+			string sql = "";
+			if (initPlayerTankId != 0)
+			{
+				sql = "select name from tank inner join playerTank on tank.id=playerTank.tankId where playerTank.id=@id; ";
+				DB.AddWithValue(ref sql, "@id", initPlayerTankId, DB.SqlDataType.Int);
+				ddTank.Text = DB.FetchData(sql).Rows[0][0].ToString();
+			}
+			else
+			{
+				ddTank.Text = "( All Tanks )";
+			}
+			// Available charts
+			AddChartValues();
+			// DropDown Tank List
+			sql = "select tank.name from tank inner join playerTank on tank.id = playerTank.tankId where playerTank.playerId=@playerId order by lastBattleTime DESC";
+			DB.AddWithValue(ref sql, "@playerId", Config.Settings.playerId, DB.SqlDataType.Int);
+			DataTable dt = DB.FetchData(sql);
+			ddTankList = "( All Tanks )";
+			foreach (DataRow dr in dt.Rows)
+			{
+				ddTankList += "," + dr["name"].ToString();
+			}
+			// DropDown Chart Values
+			foreach (ChartValue c in chartValues)
+			{
+				ddChartList += c.name + ",";
+			}
+			ddChartList = ddChartList.Substring(0, ddChartList.Length - 1); // Remove last comma
+		}
+		
+		private void DrawChart()
+		{
+			// Init
+			string tankName = ddTank.Text;
+			if (tankName == "( All Tanks )") tankName = "All Tanks";
+			string selectedChartValue = ddValue.Text;
+			string chartSerie = tankName + " - " + selectedChartValue;
+			string chartOrder = "";
+			// Check if value selected
+			if (ddValue.Text == "")
+			{
+				Code.MsgBox.Show("Please select a chart value, this is the data that shows up in the chart.", "Missing Chart Value");
+				return;
+			}
+			// Check if already shown
+			foreach (Series serie in ChartingMain.Series)
+			{
+				if (serie.Name == chartSerie) 
+					return;
+			}
+			// Get Chart Value selected
+			ChartValue chartValue = chartValues.Find(c => c.name == selectedChartValue);
+			// Add series
+			Series newSerie = new Series(chartSerie);
+			//newSerie.AxisLabel = tankName;
+			newSerie.ChartType = SeriesChartType.Line;
+			if (ddXaxis.Text == "Date")
+			{
+				ChartingMain.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Auto;
+				ChartingMain.RightToLeft = System.Windows.Forms.RightToLeft.No;
+				newSerie.XValueType = ChartValueType.DateTime;
+				newSerie.MarkerStyle = MarkerStyle.Circle;
+				chartOrder = "DESC";
+			}
+			else if (ddXaxis.Text == "Battles")
+			{
+				ChartingMain.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Number;
+				ChartingMain.RightToLeft = System.Windows.Forms.RightToLeft.No;
+				newSerie.XValueType = ChartValueType.Int32;
+				newSerie.MarkerStyle = MarkerStyle.None;
+			}
+			ChartingMain.Series.Add(newSerie);
+			// Special calculations for calculated columns
+			switch (selectedChartValue)
+			{
+				case "Win Rate": DrawChartWinRate(tankName, chartSerie, chartOrder); return;
+				case "WN8": return;
+				case "EFF": return;
+			}
+			// Find playerTank current value or all tanks current value
+			double currentValue = 0;
+			double firstValue = 0;
+			string sql = "";
+			// Get Standard battle data for non-calculated values
+			if (tankName != "All Tanks")
+			{
+				// Find playertank and current value
+				int playerTankId = TankData.GetPlayerTankId(tankName);
+				sql = "select SUM(" + chartValue.ptbCol + ") from playerTankBattle where playerTankId=@playerTankId";
+				DB.AddWithValue(ref sql, "@playerTankId", playerTankId, DB.SqlDataType.Int);
+				currentValue = Convert.ToInt32(DB.FetchData(sql).Rows[0][0]);
+				if (ddXaxis.Text == "Battles")
+				{
+					// Find first value bu sutracting sum of recorded values
+					sql = "select sum(" + chartValue.bCol + ") from battle where playerTankId=@playerTankId ";
+					DB.AddWithValue(ref sql, "@playerTankId", playerTankId, DB.SqlDataType.Int);
+					DataTable dtSum = DB.FetchData(sql);
+					if (dtSum.Rows.Count > 0)
+						if (dtSum.Rows[0][0] != DBNull.Value)
+							firstValue = currentValue - Convert.ToDouble(dtSum.Rows[0][0]);
+				}
+				// Find battles
+				sql = "select * from battle where playerTankId=@playerTankId order by battleTime " + chartOrder;
+				DB.AddWithValue(ref sql, "@playerTankId", playerTankId, DB.SqlDataType.Int);
+			}
+			else
+			{
+				// Find current value for all tanks
+				sql = "select SUM(" + chartValue.ptbCol + ") from playerTankBattle";
+				currentValue = Convert.ToInt32(DB.FetchData(sql).Rows[0][0]);
+				if (ddXaxis.Text == "Battles")
+				{
+					// Find first value bu sutracting sum of recorded values
+					sql = "select sum(" + chartValue.bCol + ") from battle ";
+					DataTable dtSum = DB.FetchData(sql);
+					if (dtSum.Rows.Count > 0)
+						firstValue = currentValue - Convert.ToDouble(dtSum.Rows[0][0]);
+				}
+				// Find battles
+				sql = "select * from battle order by battleTime " + chartOrder;
+			}
+			DataTable dtChart = DB.FetchData(sql);
+			if (ddXaxis.Text == "Date")
+			{
+				foreach (DataRow dr in dtChart.Rows)
+				{
+					ChartingMain.Series[chartSerie].Points.AddXY(Convert.ToDateTime(dr["battleTime"]), currentValue); // Use battle date
+					currentValue -= Convert.ToDouble(dr[chartValue.bCol]); //  Move backwards
+				}
+			}
+			else if (ddXaxis.Text == "Battles")
+			{
+				double battleCount = 0;
+				foreach (DataRow dr in dtChart.Rows)
+				{
+					battleCount += Convert.ToDouble(dr["battlesCount"]); // Use battle count
+					ChartingMain.Series[chartSerie].Points.AddXY(battleCount, firstValue);
+					firstValue += Convert.ToDouble(dr[chartValue.bCol]); // Move forwards
+				}
+			}
+			
+		}
+
+		private int axisYminimum = 100;
+		private void DrawChartWinRate(string tankName, string chartSerie, string chartOrder)
+		{
+			// ChartingMain.ChartAreas[0].AxisY.Interval = 1;
+			// Find playerTank current value or all tanks current value
+			double currentWins = 0;
+			double currentBattles = 0;
+			double firstWins = 0;
+			double firstBattles = 0;
+			string sql = "";
+			// Get Standard battle data for non-calculated values
+			if (tankName != "All Tanks")
+			{
+				// Find playertank and current value
+				int playerTankId = TankData.GetPlayerTankId(tankName);
+				sql = "select SUM(wins), SUM(battles) from playerTankBattle where playerTankId=@playerTankId";
+				DB.AddWithValue(ref sql, "@playerTankId", playerTankId, DB.SqlDataType.Int);
+				currentWins = Convert.ToInt32(DB.FetchData(sql).Rows[0][0]);
+				currentBattles = Convert.ToInt32(DB.FetchData(sql).Rows[0][1]);
+				if (ddXaxis.Text == "Battles")
+				{
+					// Find first value by sutracting sum of recorded values
+					sql = "select sum(victory), sum(battlescount) from battle where playerTankId=@playerTankId ";
+					DB.AddWithValue(ref sql, "@playerTankId", playerTankId, DB.SqlDataType.Int);
+					DataTable dtSum = DB.FetchData(sql);
+					if (dtSum.Rows.Count > 0)
+					{
+						if (dtSum.Rows[0][0] != DBNull.Value)
+							firstWins = currentWins - Convert.ToDouble(dtSum.Rows[0][0]);
+						if (dtSum.Rows[0][1] != DBNull.Value)
+							firstBattles = currentBattles - Convert.ToDouble(dtSum.Rows[0][1]);
+					}
+				}
+				// Find battles
+				sql = "select * from battle where playerTankId=@playerTankId order by battleTime " + chartOrder;
+				DB.AddWithValue(ref sql, "@playerTankId", playerTankId, DB.SqlDataType.Int);
+			}
+			else
+			{
+				// Find current value for all tanks
+				sql = "select SUM(wins), SUM(battles) from playerTankBattle";
+				currentWins = Convert.ToInt32(DB.FetchData(sql).Rows[0][0]);
+				currentBattles = Convert.ToInt32(DB.FetchData(sql).Rows[0][1]);
+				if (ddXaxis.Text == "Battles")
+				{
+					// Find first value by subtracting sum of recorded values
+					sql = "select SUM(victory), SUM(battlescount) from battle ";
+					DataTable dtSum = DB.FetchData(sql);
+					if (dtSum.Rows.Count > 0)
+					{
+						if (dtSum.Rows[0][0] != DBNull.Value)
+							firstWins = currentWins - Convert.ToDouble(dtSum.Rows[0][0]);
+						if (dtSum.Rows[0][1] != DBNull.Value)
+							firstBattles = currentBattles - Convert.ToDouble(dtSum.Rows[0][1]);
+					}
+				}
+				// Find battles
+				sql = "select * from battle order by battleTime " + chartOrder;
+			}
+			DataTable dtChart = DB.FetchData(sql);
+			double winRate = 0;
+			if (ddXaxis.Text == "Date")
+			{
+				foreach (DataRow dr in dtChart.Rows)
+				{
+					winRate = currentWins * 100 / currentBattles;
+					if (winRate < axisYminimum) axisYminimum = Convert.ToInt32(winRate);
+					ChartingMain.Series[chartSerie].Points.AddXY(Convert.ToDateTime(dr["battleTime"]), winRate); // Use battle date
+					currentWins -= Convert.ToDouble(dr["victory"]); //  Move backwards
+					currentBattles -= Convert.ToDouble(dr["battlesCount"]);
+				}
+			}
+			else if (ddXaxis.Text == "Battles")
+			{
+				double battleCount = 0;
+				foreach (DataRow dr in dtChart.Rows)
+				{
+					winRate = firstWins * 100 / firstBattles;
+					if (winRate < axisYminimum) axisYminimum = Convert.ToInt32(winRate);
+					battleCount += Convert.ToDouble(dr["battlesCount"]); // Use battle count
+					ChartingMain.Series[chartSerie].Points.AddXY(battleCount, winRate);
+					firstWins += Convert.ToDouble(dr["victory"]); // Move forwards
+					firstBattles += Convert.ToDouble(dr["battlesCount"]);
+				}
+			}
+			ChartingMain.ChartAreas[0].AxisY.Minimum = axisYminimum;
+		}
+
+		string selectedXaxis = "";
+		private void ddXaxis_Click(object sender, EventArgs e)
+		{
+			selectedXaxis = ddXaxis.Text;						
+			Code.DropDownGrid.Show(ddXaxis, Code.DropDownGrid.DropDownGridType.List, "Date,Battles");
+		}
+
+		private void ddXaxis_TextChanged(object sender, EventArgs e)
+		{
+			if (ChartingMain.Series.Count > 0)
+			{
+				if (selectedXaxis != ddXaxis.Text)
+				{
+					Code.MsgBox.Button answer = MsgBox.Show("Changing x-axis require chart to be cleared", "Clear Chart?", MsgBoxType.OKCancel);
+					if (answer == MsgBox.Button.OKButton)
+					{
+						ResetChart();
+					}
+					else
+					{
+						ddXaxis.Text = selectedXaxis;
+					}
+				}
+			}
+		}
+
+
+		private void btnAddChart_Click(object sender, EventArgs e)
+		{
+			DrawChart();
+		}
+
+		private void ddTank_Click(object sender, EventArgs e)
+		{
+			Code.DropDownGrid.Show(ddTank, Code.DropDownGrid.DropDownGridType.List, ddTankList);
+		}
+
+		private void ddValue_Click(object sender, EventArgs e)
+		{
+			Code.DropDownGrid.Show(ddValue, Code.DropDownGrid.DropDownGridType.List, ddChartList);
+		}
+
+		private void btnClearChart_Click(object sender, EventArgs e)
+		{
+			ResetChart();
+		}
+
+		private void ResetChart()
+		{
+			ChartingMain.Series.Clear();
+			ChartingMain.ResetAutoValues();
+			axisYminimum = 100;
+		}
+		
+	}
+}
