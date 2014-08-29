70452898097360c130b56879614b13d3a4a02df1
BadButton <kih@hestad.no>
2014-08-29 14:41:55 +0200

    Missing files

---
+++ WinApp/Gadget/ucBattleListLargeImages.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace WinApp.Gadget
+{
+	partial class ucBattleListLargeImages
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// ucBattleListLargeImages
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+			this.Name = "ucBattleListLargeImages";
+			this.Size = new System.Drawing.Size(160, 100);
+			this.Load += new System.EventHandler(this.ucImage_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+	}
+}
