8064e332588be6e218ae1fe287a3195888d5262c
BadButton <kih@hestad.no>
2014-09-01 23:28:29 +0200

    version 0.3.12

--- WinApp/Properties/AssemblyInfo.cs
+++ WinApp/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // - Second number = minor versjon
 // - Third number = not used
 // - Forth number = buid number (automaically)
-[assembly: AssemblyVersion("0.3.11.*")]
-[assembly: AssemblyFileVersion("0.3.11.0")]
+[assembly: AssemblyVersion("0.3.12.*")]
+[assembly: AssemblyFileVersion("0.3.12.0")]
