1ce7d9563541c8fd3e4e03c74c363936d2ae7bc7
BadButton <kih@hestad.no>
2014-09-10 22:36:31 +0200

    Cleanup datetimehelper

--- WinApp/Code/DateTimeHelper.cs
+++ WinApp/Code/DateTimeHelper.cs
@@ -14,21 +14,6 @@ namespace WinApp.Code
 			return origin.AddSeconds(timestamp);
 		}
 
-		//public static DataRow AdjustForTimeZone(DataRow playerTankRow)
-		//{
-		//	TimeZone currentTimeZone = TimeZone.CurrentTimeZone;
-		//	TimeSpan offset = currentTimeZone.GetUtcOffset(DateTime.Now);
-
-		//	if (playerTankRow["creationTime"] != DBNull.Value)
-		//		playerTankRow["creationTime"] = Convert.ToDateTime(playerTankRow["creationTime"]).AddHours(offset.Hours);
-		//	if (playerTankRow["updatedTime"] != DBNull.Value)
-		//		playerTankRow["updatedTime"] = Convert.ToDateTime(playerTankRow["updatedTime"]).AddHours(offset.Hours);
-		//	if (playerTankRow["lastBattleTime"] != DBNull.Value)
-		//		playerTankRow["lastBattleTime"] = Convert.ToDateTime(playerTankRow["lastBattleTime"]).AddHours(offset.Hours);
-
-		//	return playerTankRow;
-		//}
-
 		public static DateTime AdjustForTimeZone(DateTime timeToAdjust)
 		{
 			TimeZone currentTimeZone = TimeZone.CurrentTimeZone;
