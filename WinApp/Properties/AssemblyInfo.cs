55c35efeb049855aa66db04fb93ddad2cb862144
BadButton <kih@hestad.no>
2014-09-13 01:54:01 +0200

    Ver 0.4.4

--- WinApp/Properties/AssemblyInfo.cs
+++ WinApp/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // - Second number = minor versjon
 // - Third number = not used
 // - Forth number = buid number (automaically)
-[assembly: AssemblyVersion("0.4.3.*")]
-[assembly: AssemblyFileVersion("0.4.3.0")]
+[assembly: AssemblyVersion("0.4.4.*")]
+[assembly: AssemblyFileVersion("0.4.4.0")]
