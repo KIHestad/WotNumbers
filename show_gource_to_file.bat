b29719d386d0cc8aa2ce8e23577f04c7dae898d4
BadButton <kih@hestad.no>
2014-09-20 13:25:32 +0200

    file to make gource video

---
+++ show_gource_to_file.bat
@@ -0,0 +1,1 @@
+gource -a 0.1 -s 0.1 --user-image-dir C:\dev\gource -i 600 -b 000000 -title "Wot Numbers" --highlight-users --bloom-intensity 1 --hide filenames,progress --key -1280x720 -o c:\dev\gource\gource.ppm --stop-at-end -f
