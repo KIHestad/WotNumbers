32fd9dcc9de94a17b1ad7b417fc5cde7ecf3e740
BadButton <kih@hestad.no>
2014-10-06 16:41:35 +0200

    Battle detail "My Result" heading

--- WinApp/Forms/BattleDetail.Designer.cs
+++ WinApp/Forms/BattleDetail.Designer.cs
@@ -32,16 +32,34 @@
 			BadThemeContainerControl.MainAreaClass mainAreaClass1 = new BadThemeContainerControl.MainAreaClass();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BattleDetail));
 			this.BattleDetailTheme = new BadForm();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.lblDuration = new System.Windows.Forms.Label();
+			this.lblTime = new System.Windows.Forms.Label();
+			this.lblDate = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblTankName = new System.Windows.Forms.Label();
+			this.lblResult = new System.Windows.Forms.Label();
+			this.lblMap = new System.Windows.Forms.Label();
+			this.lblSurvival = new System.Windows.Forms.Label();
+			this.lblBattleMode = new System.Windows.Forms.Label();
+			this.picTank = new System.Windows.Forms.PictureBox();
 			this.btnEnemyTeam = new BadButton();
 			this.btnOurTeam = new BadButton();
 			this.btnTeams = new BadButton();
 			this.btnPersonal = new BadButton();
 			this.badGroupBox1 = new BadGroupBox();
 			this.BattleDetailTheme.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picTank)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// BattleDetailTheme
 			// 
+			this.BattleDetailTheme.Controls.Add(this.panel1);
 			this.BattleDetailTheme.Controls.Add(this.btnEnemyTeam);
 			this.BattleDetailTheme.Controls.Add(this.btnOurTeam);
 			this.BattleDetailTheme.Controls.Add(this.btnTeams);
@@ -58,13 +76,177 @@
 			this.BattleDetailTheme.MainArea = mainAreaClass1;
 			this.BattleDetailTheme.Name = "BattleDetailTheme";
 			this.BattleDetailTheme.Resizable = true;
-			this.BattleDetailTheme.Size = new System.Drawing.Size(701, 427);
+			this.BattleDetailTheme.Size = new System.Drawing.Size(700, 550);
 			this.BattleDetailTheme.SystemExitImage = ((System.Drawing.Image)(resources.GetObject("BattleDetailTheme.SystemExitImage")));
 			this.BattleDetailTheme.SystemMaximizeImage = null;
 			this.BattleDetailTheme.SystemMinimizeImage = null;
 			this.BattleDetailTheme.TabIndex = 0;
 			this.BattleDetailTheme.Text = "Battle Details";
 			this.BattleDetailTheme.TitleHeight = 26;
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Location = new System.Drawing.Point(26, 64);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(648, 455);
+			this.panel1.TabIndex = 6;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+			this.panel2.Controls.Add(this.lblDuration);
+			this.panel2.Controls.Add(this.lblTime);
+			this.panel2.Controls.Add(this.lblDate);
+			this.panel2.Controls.Add(this.label3);
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Controls.Add(this.lblTankName);
+			this.panel2.Controls.Add(this.lblResult);
+			this.panel2.Controls.Add(this.lblMap);
+			this.panel2.Controls.Add(this.lblSurvival);
+			this.panel2.Controls.Add(this.lblBattleMode);
+			this.panel2.Controls.Add(this.picTank);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(648, 100);
+			this.panel2.TabIndex = 6;
+			// 
+			// lblDuration
+			// 
+			this.lblDuration.AutoSize = true;
+			this.lblDuration.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.lblDuration.Location = new System.Drawing.Point(256, 76);
+			this.lblDuration.Name = "lblDuration";
+			this.lblDuration.Size = new System.Drawing.Size(42, 13);
+			this.lblDuration.TabIndex = 13;
+			this.lblDuration.Text = "MM:SS";
+			this.lblDuration.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// lblTime
+			// 
+			this.lblTime.AutoSize = true;
+			this.lblTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.lblTime.Location = new System.Drawing.Point(256, 58);
+			this.lblTime.Name = "lblTime";
+			this.lblTime.Size = new System.Drawing.Size(44, 13);
+			this.lblTime.TabIndex = 12;
+			this.lblTime.Text = "HH:MM";
+			this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// lblDate
+			// 
+			this.lblDate.AutoSize = true;
+			this.lblDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.lblDate.Location = new System.Drawing.Point(256, 40);
+			this.lblDate.Name = "lblDate";
+			this.lblDate.Size = new System.Drawing.Size(75, 13);
+			this.lblDate.TabIndex = 11;
+			this.lblDate.Text = "DD.MM.YYYY";
+			this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.label3.Location = new System.Drawing.Point(205, 76);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(50, 13);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "Duration:";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.label2.Location = new System.Drawing.Point(205, 58);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(33, 13);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Time:";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.label1.Location = new System.Drawing.Point(205, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(33, 13);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Date:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// lblTankName
+			// 
+			this.lblTankName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.lblTankName.Location = new System.Drawing.Point(254, 11);
+			this.lblTankName.Name = "lblTankName";
+			this.lblTankName.Size = new System.Drawing.Size(273, 24);
+			this.lblTankName.TabIndex = 7;
+			this.lblTankName.Text = "Tank Name";
+			this.lblTankName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// lblResult
+			// 
+			this.lblResult.AutoSize = true;
+			this.lblResult.BackColor = System.Drawing.Color.Transparent;
+			this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResult.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.lblResult.Location = new System.Drawing.Point(12, 11);
+			this.lblResult.Name = "lblResult";
+			this.lblResult.Size = new System.Drawing.Size(112, 24);
+			this.lblResult.TabIndex = 2;
+			this.lblResult.Text = "Battle Result";
+			this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// lblMap
+			// 
+			this.lblMap.AutoSize = true;
+			this.lblMap.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.lblMap.Location = new System.Drawing.Point(13, 76);
+			this.lblMap.Name = "lblMap";
+			this.lblMap.Size = new System.Drawing.Size(57, 13);
+			this.lblMap.TabIndex = 6;
+			this.lblMap.Text = "Map name";
+			this.lblMap.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// lblSurvival
+			// 
+			this.lblSurvival.AutoSize = true;
+			this.lblSurvival.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.lblSurvival.Location = new System.Drawing.Point(13, 40);
+			this.lblSurvival.Name = "lblSurvival";
+			this.lblSurvival.Size = new System.Drawing.Size(119, 13);
+			this.lblSurvival.TabIndex = 4;
+			this.lblSurvival.Text = "Survival / Extermination";
+			this.lblSurvival.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// lblBattleMode
+			// 
+			this.lblBattleMode.AutoSize = true;
+			this.lblBattleMode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(187)))));
+			this.lblBattleMode.Location = new System.Drawing.Point(13, 58);
+			this.lblBattleMode.Name = "lblBattleMode";
+			this.lblBattleMode.Size = new System.Drawing.Size(64, 13);
+			this.lblBattleMode.TabIndex = 5;
+			this.lblBattleMode.Text = "Battle Mode";
+			this.lblBattleMode.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// picTank
+			// 
+			this.picTank.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.picTank.Location = new System.Drawing.Point(478, 0);
+			this.picTank.Name = "picTank";
+			this.picTank.Size = new System.Drawing.Size(170, 100);
+			this.picTank.TabIndex = 1;
+			this.picTank.TabStop = false;
 			// 
 			// btnEnemyTeam
 			// 
@@ -127,7 +309,7 @@
 			this.badGroupBox1.Image = null;
 			this.badGroupBox1.Location = new System.Drawing.Point(25, 56);
 			this.badGroupBox1.Name = "badGroupBox1";
-			this.badGroupBox1.Size = new System.Drawing.Size(651, 342);
+			this.badGroupBox1.Size = new System.Drawing.Size(650, 465);
 			this.badGroupBox1.TabIndex = 0;
 			// 
 			// BattleDetail
@@ -135,7 +317,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.Fuchsia;
-			this.ClientSize = new System.Drawing.Size(701, 427);
+			this.ClientSize = new System.Drawing.Size(700, 550);
 			this.Controls.Add(this.BattleDetailTheme);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.MinimumSize = new System.Drawing.Size(600, 300);
@@ -143,7 +325,14 @@
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "BattleDetail";
 			this.TransparencyKey = System.Drawing.Color.Fuchsia;
+			this.Load += new System.EventHandler(this.BattleDetail_Load);
+			this.ResizeEnd += new System.EventHandler(this.BattleDetail_Resize);
+			this.Resize += new System.EventHandler(this.BattleDetail_Resize);
 			this.BattleDetailTheme.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picTank)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -156,5 +345,19 @@
 		private BadButton btnTeams;
 		private BadButton btnPersonal;
 		private BadGroupBox badGroupBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox picTank;
+		private System.Windows.Forms.Label lblResult;
+		private System.Windows.Forms.Label lblSurvival;
+		private System.Windows.Forms.Label lblBattleMode;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label lblMap;
+		private System.Windows.Forms.Label lblTankName;
+		private System.Windows.Forms.Label lblDuration;
+		private System.Windows.Forms.Label lblTime;
+		private System.Windows.Forms.Label lblDate;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
 	}
 }
--- WinApp/Forms/BattleDetail.cs
+++ WinApp/Forms/BattleDetail.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using WinApp.Code;
 
 namespace WinApp.Forms
 {
@@ -21,12 +22,122 @@ namespace WinApp.Forms
 
 		private void btnTab_Click(object sender, EventArgs e)
 		{
+			// deselect tabs
 			btnEnemyTeam.Checked = false;
 			btnOurTeam.Checked = false;
 			btnPersonal.Checked = false;
 			btnTeams.Checked = false;
+			// deselect panels
+			panel1.Visible = false;
+			// select tab
 			BadButton btn = (BadButton)sender;
 			btn.Checked = true;
+			string selectedTab = btn.Name;
+			switch (selectedTab)
+			{
+				case "btnPersonal" :
+					panel1.Visible = true;
+					break;
+			}
 		}
+
+		private void BattleDetail_Load(object sender, EventArgs e)
+		{
+			GetMyPersonalInfo();
+		}
+
+		private void GetMyPersonalInfo()
+		{
+			string sql =
+				"SELECT battle.*, tank.id as tankId, tank.name as tankName, map.name as mapName, " +
+				"		battleResult.name as battleResultName, battleResult.color as battleResultColor, " + 
+				"		battleSurvive.name as battleSurviveName, battleSurvive.color as battleSurviveColor " +
+				"FROM   battle INNER JOIN " +
+				"       playerTank ON battle.playerTankId = playerTank.id INNER JOIN " +
+				"       tank ON playerTank.tankId = tank.id INNER JOIN " +
+				"       tankType ON tank.tankTypeId = tankType.Id INNER JOIN " +
+				"       country ON tank.countryId = country.Id INNER JOIN " +
+				"       battleResult ON battle.battleResultId = battleResult.id LEFT JOIN " +
+				"       map on battle.mapId = map.id INNER JOIN " +
+				"       battleSurvive ON battle.battleSurviveId = battleSurvive.id " +
+				"WHERE	battle.id=@battleId";
+			DB.AddWithValue(ref sql, "@battleId", _battleId, DB.SqlDataType.Int);
+			DataTable dt = DB.FetchData(sql);
+			if (dt.Rows.Count > 0)
+			{
+				DataRow dr = dt.Rows[0];
+				// Battle result
+				lblResult.Text = dr["battleResultName"].ToString();
+				Color battleResultColor = ColorTranslator.FromHtml(dr["battleResultColor"].ToString());
+				lblResult.ForeColor = battleResultColor;
+				// Tank img
+				int tankId = Convert.ToInt32(dr["tankId"]);
+				Image tankImg = ImageHelper.GetTankImage(tankId, ImageHelper.TankImageType.LargeImage);
+				picTank.Image = tankImg;
+				// Tank name
+				lblTankName.Text = dr["tankName"].ToString();
+				// Map name
+				string mapName = "";
+				if (dr["mapName"] != DBNull.Value) mapName = dr["mapName"].ToString();
+				lblMap.Text = mapName;
+				// Battle time
+				DateTime finished = Convert.ToDateTime(dr["battleTime"]);
+				int duration = Convert.ToInt32(dr["battleLifeTime"]);
+				TimeSpan t = TimeSpan.FromSeconds(duration);
+				lblDate.Text = finished.ToString("d");
+				lblTime.Text = finished.ToString("t");
+				lblDuration.Text = string.Format("{0:D0}:{1:D2}", t.Minutes, t.Seconds);
+				// Battle count
+				int battleCount = Convert.ToInt32(dr["battlesCount"]);
+				int survivedCount = Convert.ToInt32(dr["survived"]);
+				// Survival
+				string survival = dr["battleSurviveName"].ToString();
+				string deathReason = "";
+				if (dr["deathReason"] != DBNull.Value && survival != "Yes") deathReason = " due to: " + dr["deathReason"].ToString();
+				Color battleSurviveColor = ColorTranslator.FromHtml(dr["battleSurviveColor"].ToString());
+				switch (survival)
+				{
+					case "Yes": survival = "Survived"; break;
+					case "No": survival = "Killed" + deathReason; break;
+					case "Some": survival = "Survived: " + survivedCount.ToString() + " / " + battleCount.ToString(); break;
+				}
+				lblSurvival.Text = survival;
+				lblSurvival.ForeColor = battleSurviveColor;
+				
+				// Battle mode
+				string battleMode = "";
+				int bonusType = -1;
+				if (dr["bonusType"] != DBNull.Value) bonusType = Convert.ToInt32(dr["bonusType"]);
+				if (bonusType == -1)
+				{
+					if (battleCount > 1)
+						battleMode = "Several battles recorded";
+					else
+						battleMode = "Battle result not fetched";
+				}
+				else
+				{
+					switch (bonusType)
+					{
+						case 0: battleMode = "Unknown Battle Mode"; break;
+						case 1: battleMode = "Standard Battle"; break;
+						case 2: battleMode = "Trainig Room Battle"; break;
+						case 3: battleMode = "Tank Company Battle"; break;
+						case 4: battleMode = "Clan War Battle"; break;
+						case 5: battleMode = "Tutorial Battle"; break;
+						case 10: battleMode = "Skimish Battle"; break;
+					}
+				}
+				lblBattleMode.Text = battleMode;
+			}
+		}
+
+		private void BattleDetail_Resize(object sender, EventArgs e)
+		{
+			//lblResult.Left = panel1.Width / 2 - lblResult.Width / 2;
+			//picTank.Left = panel1.Width / 2 - picTank.Width / 2;
+		}
+
+
 	}
 }
