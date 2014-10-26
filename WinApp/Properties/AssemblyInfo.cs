f2fd930b10985ce67a8409a4145233b9c1418b0a
BadButton <kih@hestad.no>
2014-10-26 21:10:26 +0100

    New version 0.5.11

--- WinApp/Properties/AssemblyInfo.cs
+++ WinApp/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // - Second number = minor versjon
 // - Third number = not used
 // - Forth number = buid number (automaically)
-[assembly: AssemblyVersion("0.5.10.*")]
-[assembly: AssemblyFileVersion("0.5.10.0")]
+[assembly: AssemblyVersion("0.5.11.*")]
+[assembly: AssemblyFileVersion("0.5.11.0")]
