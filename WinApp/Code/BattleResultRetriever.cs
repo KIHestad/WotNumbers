fb46250a36186b1925db019640ca459dc38b0615
BadButton <kih@hestad.no>
2015-05-23 16:46:23 +0200

    BRR improvements, 0.8.9 released to test pilots

--- WinApp/Code/BattleResultRetriever.cs
+++ WinApp/Code/BattleResultRetriever.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Permissions;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,25 +13,37 @@ namespace WinApp.Code
 	{
 		public static string BrrFile
 		{
-			get {return BrrFolder + "BRR.pyc";}
+			get {return modsFolder + "BRR.pyc";}
 			set {}
 		}
 
 		public static string InitFile
 		{
-			get {return BrrFolder + "__init__.pyc";}
+			get {return modsFolder + "__init__.pyc";}
 			set { }
 		}
 
-		public static string BrrFolder
+		public static string CameraNodeFile
+		{
+			get { return clientFolder + "CameraNode.pyc"; }
+			set { }
+		}
+
+		public static string clientFolder
 		{
 			get
 			{
 				string wotFolder = Config.Settings.wotGameFolder;
 				if (wotFolder != "" && wotFolder.Substring(wotFolder.Length - 1, 1) != "\\")
 					wotFolder += "\\";
-				return wotFolder += "res_mods\\0.9.7\\scripts\\client\\mods\\";
+				return wotFolder += "res_mods\\0.9.7\\scripts\\client\\";
 			}
+			set { }
+		}
+
+		public static string modsFolder
+		{
+			get {return clientFolder += "mods\\";}
 			set {}
 		}
 
@@ -66,7 +80,9 @@ namespace WinApp.Code
 					files++;
 				if (File.Exists(BattleResultRetriever.InitFile))
 					files++;
-				return (files == 2);
+				if (File.Exists(BattleResultRetriever.CameraNodeFile))
+					files++;
+				return (files == 3);
 			}
 			set {}
 		}
@@ -78,8 +94,11 @@ namespace WinApp.Code
 			msg = "";
 			try
 			{
-				if (!Directory.Exists(BrrFolder))
-					Directory.CreateDirectory(BrrFolder);
+				if (!Directory.Exists(modsFolder))
+				{
+					Directory.CreateDirectory(modsFolder);
+				}
+				string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
 				if (!File.Exists(BrrFile))
 				{
 					string fileToCopy = Path.GetDirectoryName(Application.ExecutablePath) + "\\Docs\\BRR.pyc";
@@ -90,6 +109,15 @@ namespace WinApp.Code
 					string fileToCopy = Path.GetDirectoryName(Application.ExecutablePath) + "\\Docs\\__init__.pyc";
 					File.Copy(fileToCopy, InitFile);
 				}
+				if (!File.Exists(CameraNodeFile))
+				{
+					string fileToCopy = Path.GetDirectoryName(Application.ExecutablePath) + "\\Docs\\CameraNode.pyc";
+					File.Copy(fileToCopy, CameraNodeFile);
+				}
+				// Add the access control entry to the files
+				AddFileSecurity(BrrFile, userName, FileSystemRights.FullControl, AccessControlType.Allow);
+				AddFileSecurity(CameraNodeFile, userName, FileSystemRights.FullControl, AccessControlType.Allow);
+				AddFileSecurity(InitFile, userName, FileSystemRights.FullControl, AccessControlType.Allow);
 			}
 			catch (Exception ex)
 			{
@@ -115,7 +143,27 @@ namespace WinApp.Code
 			return ok;
 		}
 
-		
+		// Adds an ACL entry on the specified file for the specified account. 
+		public static void AddFileSecurity(string fileName, string account, FileSystemRights rights, AccessControlType controlType)
+		{
+			try
+			{
+				// Get a FileSecurity object that represents the 
+				// current security settings.
+				FileSecurity fSecurity = File.GetAccessControl(fileName);
+
+				// Add the FileSystemAccessRule to the security settings.
+				fSecurity.AddAccessRule(new FileSystemAccessRule(account, rights, controlType));
+
+				// Set the new access settings.
+				File.SetAccessControl(fileName, fSecurity);
+			}
+			catch (Exception)
+			{
+				
+			}
+			
+		}
 
 	}
 }
