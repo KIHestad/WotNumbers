e6f00469d65f102e58a890460e96ae40b6c7f86d
BadButton <kih@hestad.no>
2015-09-22 16:12:39 +0200

    div files not neccesary

--- show_gource_to_file.bat
+++
@@ -1,1 +0,0 @@
-gource -a 0.1 -s 0.1 --user-image-dir C:\dev\gource -i 600 -b 000000 -title "Wot Numbers" --highlight-users --bloom-intensity 1 --hide filenames,progress --key -1280x720 -o c:\dev\gource\gource.ppm --stop-at-end -f
