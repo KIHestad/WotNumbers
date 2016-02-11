350cc8912c13a3d06a4c70016b4e18734bcf2724
BadButton <kih@hestad.no>
2016-02-11 23:33:41 +0100

    framework search tank

--- WinApp/Forms/TankSearch.Designer.cs
+++ WinApp/Forms/TankSearch.Designer.cs
@@ -32,17 +32,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TankSearch));
             this.TankSearchTheme = new BadForm();
             this.toolStripMain = new System.Windows.Forms.ToolStrip();
-            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.txtSearchText = new System.Windows.Forms.ToolStripTextBox();
-            this.toolAllTanks_Nation0 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolAllTanks_Nation1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolAllTanks_Nation2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolAllTanks_Nation3 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolAllTanks_Nation4 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolAllTanks_Nation5 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolAllTanks_Nation6 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolAllTanks_Nation7 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolAllTanks_Toggle = new System.Windows.Forms.ToolStripButton();
+            this.mLabel = new System.Windows.Forms.ToolStripLabel();
+            this.mTxtSearch = new System.Windows.Forms.ToolStripTextBox();
+            this.mNation0 = new System.Windows.Forms.ToolStripButton();
+            this.mNation1 = new System.Windows.Forms.ToolStripButton();
+            this.mNation2 = new System.Windows.Forms.ToolStripButton();
+            this.mNation3 = new System.Windows.Forms.ToolStripButton();
+            this.mNation4 = new System.Windows.Forms.ToolStripButton();
+            this.mNation5 = new System.Windows.Forms.ToolStripButton();
+            this.mNation6 = new System.Windows.Forms.ToolStripButton();
+            this.mNation7 = new System.Windows.Forms.ToolStripButton();
+            this.mNationSelectMode = new System.Windows.Forms.ToolStripButton();
+            this.mNationToggleAll = new System.Windows.Forms.ToolStripButton();
             this.TankSearchTheme.SuspendLayout();
             this.toolStripMain.SuspendLayout();
             this.SuspendLayout();
@@ -57,7 +58,7 @@
             this.TankSearchTheme.FormFooterHeight = 26;
             this.TankSearchTheme.FormInnerBorder = 3;
             this.TankSearchTheme.FormMargin = 0;
-            this.TankSearchTheme.Image = null;
+            this.TankSearchTheme.Image = ((System.Drawing.Image)(resources.GetObject("TankSearchTheme.Image")));
             this.TankSearchTheme.Location = new System.Drawing.Point(0, 0);
             this.TankSearchTheme.MainArea = mainAreaClass1;
             this.TankSearchTheme.Name = "TankSearchTheme";
@@ -76,105 +77,139 @@
             this.toolStripMain.Dock = System.Windows.Forms.DockStyle.None;
             this.toolStripMain.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripLabel1,
-            this.txtSearchText,
-            this.toolAllTanks_Nation0,
-            this.toolAllTanks_Nation1,
-            this.toolAllTanks_Nation2,
-            this.toolAllTanks_Nation3,
-            this.toolAllTanks_Nation4,
-            this.toolAllTanks_Nation5,
-            this.toolAllTanks_Nation6,
-            this.toolAllTanks_Nation7,
-            this.toolAllTanks_Toggle});
+            this.mLabel,
+            this.mTxtSearch,
+            this.mNation0,
+            this.mNation1,
+            this.mNation2,
+            this.mNation3,
+            this.mNation4,
+            this.mNation5,
+            this.mNation6,
+            this.mNation7,
+            this.mNationSelectMode,
+            this.mNationToggleAll});
             this.toolStripMain.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
-            this.toolStripMain.Location = new System.Drawing.Point(2, 29);
+            this.toolStripMain.Location = new System.Drawing.Point(2, 27);
             this.toolStripMain.Name = "toolStripMain";
             this.toolStripMain.Size = new System.Drawing.Size(808, 25);
             this.toolStripMain.Stretch = true;
             this.toolStripMain.TabIndex = 7;
             this.toolStripMain.Text = "toolStrip1";
             // 
-            // toolStripLabel1
+            // mLabel
             // 
-            this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(73, 22);
-            this.toolStripLabel1.Text = " Tank Name:";
+            this.mLabel.Name = "mLabel";
+            this.mLabel.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
+            this.mLabel.Size = new System.Drawing.Size(76, 22);
+            this.mLabel.Text = "  Tank Name:";
             // 
-            // txtSearchText
+            // mTxtSearch
             // 
-            this.txtSearchText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(49)))));
-            this.txtSearchText.ForeColor = System.Drawing.SystemColors.Window;
-            this.txtSearchText.Name = "txtSearchText";
-            this.txtSearchText.Size = new System.Drawing.Size(100, 25);
+            this.mTxtSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(49)))));
+            this.mTxtSearch.ForeColor = System.Drawing.SystemColors.Window;
+            this.mTxtSearch.Name = "mTxtSearch";
+            this.mTxtSearch.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
+            this.mTxtSearch.Size = new System.Drawing.Size(100, 25);
+            this.mTxtSearch.TextChanged += new System.EventHandler(this.mTxtSearch_TextChanged);
             // 
-            // toolAllTanks_Nation0
+            // mNation0
             // 
-            this.toolAllTanks_Nation0.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Nation0.Image")));
-            this.toolAllTanks_Nation0.Name = "toolAllTanks_Nation0";
-            this.toolAllTanks_Nation0.Size = new System.Drawing.Size(74, 25);
-            this.toolAllTanks_Nation0.Text = "U.S.S.R.";
+            this.mNation0.Image = ((System.Drawing.Image)(resources.GetObject("mNation0.Image")));
+            this.mNation0.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNation0.Name = "mNation0";
+            this.mNation0.Size = new System.Drawing.Size(66, 22);
+            this.mNation0.Text = "U.S.S.R.";
+            this.mNation0.Click += new System.EventHandler(this.mNation_Click);
             // 
-            // toolAllTanks_Nation1
+            // mNation1
             // 
-            this.toolAllTanks_Nation1.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Nation1.Image")));
-            this.toolAllTanks_Nation1.Name = "toolAllTanks_Nation1";
-            this.toolAllTanks_Nation1.Size = new System.Drawing.Size(83, 25);
-            this.toolAllTanks_Nation1.Text = "Germany";
+            this.mNation1.Image = ((System.Drawing.Image)(resources.GetObject("mNation1.Image")));
+            this.mNation1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNation1.Name = "mNation1";
+            this.mNation1.Size = new System.Drawing.Size(75, 22);
+            this.mNation1.Text = "Germany";
+            this.mNation1.Click += new System.EventHandler(this.mNation_Click);
             // 
-            // toolAllTanks_Nation2
+            // mNation2
             // 
-            this.toolAllTanks_Nation2.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Nation2.Image")));
-            this.toolAllTanks_Nation2.Name = "toolAllTanks_Nation2";
-            this.toolAllTanks_Nation2.Size = new System.Drawing.Size(66, 25);
-            this.toolAllTanks_Nation2.Text = "U.S.A.";
+            this.mNation2.Image = ((System.Drawing.Image)(resources.GetObject("mNation2.Image")));
+            this.mNation2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNation2.Name = "mNation2";
+            this.mNation2.Size = new System.Drawing.Size(58, 22);
+            this.mNation2.Text = "U.S.A.";
+            this.mNation2.Click += new System.EventHandler(this.mNation_Click);
             // 
-            // toolAllTanks_Nation3
+            // mNation3
             // 
-            this.toolAllTanks_Nation3.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Nation3.Image")));
-            this.toolAllTanks_Nation3.Name = "toolAllTanks_Nation3";
-            this.toolAllTanks_Nation3.Size = new System.Drawing.Size(66, 25);
-            this.toolAllTanks_Nation3.Text = "China";
+            this.mNation3.Image = ((System.Drawing.Image)(resources.GetObject("mNation3.Image")));
+            this.mNation3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNation3.Name = "mNation3";
+            this.mNation3.Size = new System.Drawing.Size(58, 22);
+            this.mNation3.Text = "China";
+            this.mNation3.Click += new System.EventHandler(this.mNation_Click);
             // 
-            // toolAllTanks_Nation4
+            // mNation4
             // 
-            this.toolAllTanks_Nation4.BackColor = System.Drawing.SystemColors.Control;
-            this.toolAllTanks_Nation4.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Nation4.Image")));
-            this.toolAllTanks_Nation4.Name = "toolAllTanks_Nation4";
-            this.toolAllTanks_Nation4.Size = new System.Drawing.Size(70, 25);
-            this.toolAllTanks_Nation4.Text = "France";
+            this.mNation4.Image = ((System.Drawing.Image)(resources.GetObject("mNation4.Image")));
+            this.mNation4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNation4.Name = "mNation4";
+            this.mNation4.Size = new System.Drawing.Size(62, 22);
+            this.mNation4.Text = "France";
+            this.mNation4.Click += new System.EventHandler(this.mNation_Click);
             // 
-            // toolAllTanks_Nation5
+            // mNation5
             // 
-            this.toolAllTanks_Nation5.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Nation5.Image")));
-            this.toolAllTanks_Nation5.Name = "toolAllTanks_Nation5";
-            this.toolAllTanks_Nation5.Size = new System.Drawing.Size(56, 25);
-            this.toolAllTanks_Nation5.Text = "U.K.";
+            this.mNation5.Image = ((System.Drawing.Image)(resources.GetObject("mNation5.Image")));
+            this.mNation5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNation5.Name = "mNation5";
+            this.mNation5.Size = new System.Drawing.Size(48, 22);
+            this.mNation5.Text = "U.K.";
+            this.mNation5.Click += new System.EventHandler(this.mNation_Click);
             // 
-            // toolAllTanks_Nation6
+            // mNation6
             // 
-            this.toolAllTanks_Nation6.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Nation6.Image")));
-            this.toolAllTanks_Nation6.Name = "toolAllTanks_Nation6";
-            this.toolAllTanks_Nation6.Size = new System.Drawing.Size(65, 25);
-            this.toolAllTanks_Nation6.Text = "Japan";
+            this.mNation6.Image = ((System.Drawing.Image)(resources.GetObject("mNation6.Image")));
+            this.mNation6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNation6.Name = "mNation6";
+            this.mNation6.Size = new System.Drawing.Size(57, 22);
+            this.mNation6.Text = "Japan";
+            this.mNation6.Click += new System.EventHandler(this.mNation_Click);
             // 
-            // toolAllTanks_Nation7
+            // mNation7
             // 
-            this.toolAllTanks_Nation7.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Nation7.Image")));
-            this.toolAllTanks_Nation7.Name = "toolAllTanks_Nation7";
-            this.toolAllTanks_Nation7.Size = new System.Drawing.Size(67, 25);
-            this.toolAllTanks_Nation7.Text = "Czech";
+            this.mNation7.Image = ((System.Drawing.Image)(resources.GetObject("mNation7.Image")));
+            this.mNation7.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNation7.Name = "mNation7";
+            this.mNation7.Size = new System.Drawing.Size(59, 22);
+            this.mNation7.Text = "Czech";
+            this.mNation7.Click += new System.EventHandler(this.mNation_Click);
             // 
-            // toolAllTanks_Toggle
+            // mNationSelectMode
             // 
-            this.toolAllTanks_Toggle.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolAllTanks_Toggle.Image = ((System.Drawing.Image)(resources.GetObject("toolAllTanks_Toggle.Image")));
-            this.toolAllTanks_Toggle.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolAllTanks_Toggle.Name = "toolAllTanks_Toggle";
-            this.toolAllTanks_Toggle.Padding = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.toolAllTanks_Toggle.Size = new System.Drawing.Size(33, 22);
-            this.toolAllTanks_Toggle.Text = "All";
-            this.toolAllTanks_Toggle.ToolTipText = "Select All Nations";
+            this.mNationSelectMode.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.mNationSelectMode.Image = ((System.Drawing.Image)(resources.GetObject("mNationSelectMode.Image")));
+            this.mNationSelectMode.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNationSelectMode.Name = "mNationSelectMode";
+            this.mNationSelectMode.Size = new System.Drawing.Size(43, 22);
+            this.mNationSelectMode.Text = "Single";
+            this.mNationSelectMode.ToolTipText = "Toggle selection mode";
+            this.mNationSelectMode.Click += new System.EventHandler(this.mNationSelectMode_Click);
+            // 
+            // mNationToggleAll
+            // 
+            this.mNationToggleAll.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.mNationToggleAll.Image = ((System.Drawing.Image)(resources.GetObject("mNationToggleAll.Image")));
+            this.mNationToggleAll.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.mNationToggleAll.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mNationToggleAll.Margin = new System.Windows.Forms.Padding(0, 2, 0, 2);
+            this.mNationToggleAll.Name = "mNationToggleAll";
+            this.mNationToggleAll.Padding = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.mNationToggleAll.Size = new System.Drawing.Size(33, 21);
+            this.mNationToggleAll.Text = "All";
+            this.mNationToggleAll.ToolTipText = "Select or deselect all Nations";
+            this.mNationToggleAll.Visible = false;
+            this.mNationToggleAll.Click += new System.EventHandler(this.mNationToggleAll_Click);
             // 
             // TankSearch
             // 
@@ -188,6 +223,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "TankSearch";
             this.Load += new System.EventHandler(this.TankSearch_Load);
+            this.Resize += new System.EventHandler(this.TankSearch_Resize);
             this.TankSearchTheme.ResumeLayout(false);
             this.toolStripMain.ResumeLayout(false);
             this.toolStripMain.PerformLayout();
@@ -199,16 +235,17 @@
 
         private BadForm TankSearchTheme;
         private System.Windows.Forms.ToolStrip toolStripMain;
-        private System.Windows.Forms.ToolStripMenuItem toolAllTanks_Nation0;
-        private System.Windows.Forms.ToolStripMenuItem toolAllTanks_Nation1;
-        private System.Windows.Forms.ToolStripMenuItem toolAllTanks_Nation2;
-        private System.Windows.Forms.ToolStripMenuItem toolAllTanks_Nation3;
-        private System.Windows.Forms.ToolStripMenuItem toolAllTanks_Nation4;
-        private System.Windows.Forms.ToolStripMenuItem toolAllTanks_Nation5;
-        private System.Windows.Forms.ToolStripMenuItem toolAllTanks_Nation6;
-        private System.Windows.Forms.ToolStripMenuItem toolAllTanks_Nation7;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-        private System.Windows.Forms.ToolStripTextBox txtSearchText;
-        private System.Windows.Forms.ToolStripButton toolAllTanks_Toggle;
+        private System.Windows.Forms.ToolStripLabel mLabel;
+        private System.Windows.Forms.ToolStripTextBox mTxtSearch;
+        private System.Windows.Forms.ToolStripButton mNationToggleAll;
+        private System.Windows.Forms.ToolStripButton mNation7;
+        private System.Windows.Forms.ToolStripButton mNation0;
+        private System.Windows.Forms.ToolStripButton mNation1;
+        private System.Windows.Forms.ToolStripButton mNation2;
+        private System.Windows.Forms.ToolStripButton mNation3;
+        private System.Windows.Forms.ToolStripButton mNation4;
+        private System.Windows.Forms.ToolStripButton mNation5;
+        private System.Windows.Forms.ToolStripButton mNation6;
+        private System.Windows.Forms.ToolStripButton mNationSelectMode;
     }
 }
--- WinApp/Forms/TankSearch.cs
+++ WinApp/Forms/TankSearch.cs
@@ -21,6 +21,92 @@ namespace WinApp.Forms
         {
             // Style toolbar
             toolStripMain.Renderer = new StripRenderer();
+            toolStripMain.Left = 1;
         }
+
+        private void TankSearch_Resize(object sender, EventArgs e)
+        {
+            toolStripMain.Width = this.Width - 2;
+        }
+
+        private void mNationToggleAll_Click(object sender, EventArgs e)
+        {
+            if (mNationToggleAll.Text == "All")
+            {
+                mNationToggleAll.Text = "None";
+                SelectAllNations(true);
+            }
+            else
+            {
+                mNationToggleAll.Text = "All";
+                SelectAllNations(false);
+            }
+            SearchNow();
+        }
+
+        private void mNation_Click(object sender, EventArgs e)
+        {
+            ToolStripButton btn = (ToolStripButton)sender;
+            if (mNationSelectMode.Text == "Single" && !btn.Checked)
+                SelectAllNations(false);
+            btn.Checked = !btn.Checked;
+            SearchNow();
+        }
+
+        private int GetNationCheckedCount()
+        {
+            int count = 0;
+            for (int i = 0; i <= 7; i++)
+            {
+                ToolStripButton item = (ToolStripButton)toolStripMain.Items.Find("mNation" + i.ToString(), false)[0];
+                if (item.Checked)
+                    count++;
+            }
+            return count;
+        }
+
+        private void SelectAllNations(bool select)
+        {
+            for (int i = 0; i <= 7; i++)
+            {
+                ToolStripButton item = (ToolStripButton)toolStripMain.Items.Find("mNation" + i.ToString(), false)[0];
+                item.Checked = select;
+            }
+        }
+
+        private void mTxtSearch_TextChanged(object sender, EventArgs e)
+        {
+            SearchNow();
+        }
+
+        private void mNationSelectMode_Click(object sender, EventArgs e)
+        {
+            if (mNationSelectMode.Text == "Single")
+            {
+                mNationSelectMode.Text = "Multi";
+                mNationToggleAll.Visible = true;
+            }
+            else
+            {
+                mNationSelectMode.Text = "Single";
+                mNationToggleAll.Visible = false;
+                mNationToggleAll.Text = "All";
+                if (GetNationCheckedCount() > 1)
+                    SelectAllNations(false);
+            }
+        }
+
+        private void SearchNow()
+        {
+
+        }
+
+        private void toolAllTanks_Toggle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        
     }
 }
