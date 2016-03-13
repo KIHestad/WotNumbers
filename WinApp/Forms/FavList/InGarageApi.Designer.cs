cd608ff98c64f6c15715e7ce02e2406b2e559f02
BadButton <kih@hestad.no>
2016-03-14 00:38:02 +0100

    Changed to Chromium Web browser

--- WinApp/Forms/FavList/InGarageApi.Designer.cs
+++ WinApp/Forms/FavList/InGarageApi.Designer.cs
@@ -32,15 +32,16 @@
             BadThemeContainerControl.MainAreaClass mainAreaClass1 = new BadThemeContainerControl.MainAreaClass();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InGarageApi));
             this.badForm1 = new BadForm();
+            this.pnlBrowser = new System.Windows.Forms.Panel();
             this.txtAddress = new BadTextBox();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.badForm1.SuspendLayout();
             this.SuspendLayout();
             // 
             // badForm1
             // 
+            this.badForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.badForm1.Controls.Add(this.pnlBrowser);
             this.badForm1.Controls.Add(this.txtAddress);
-            this.badForm1.Controls.Add(this.webBrowser1);
             this.badForm1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.badForm1.FormBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.badForm1.FormExitAsMinimize = false;
@@ -51,15 +52,27 @@
             this.badForm1.Image = null;
             this.badForm1.Location = new System.Drawing.Point(0, 0);
             this.badForm1.MainArea = mainAreaClass1;
+            this.badForm1.MinimumSize = new System.Drawing.Size(380, 500);
             this.badForm1.Name = "badForm1";
             this.badForm1.Resizable = true;
-            this.badForm1.Size = new System.Drawing.Size(818, 886);
+            this.badForm1.Size = new System.Drawing.Size(606, 678);
             this.badForm1.SystemExitImage = ((System.Drawing.Image)(resources.GetObject("badForm1.SystemExitImage")));
             this.badForm1.SystemMaximizeImage = null;
             this.badForm1.SystemMinimizeImage = null;
             this.badForm1.TabIndex = 0;
             this.badForm1.Text = "Login to Wargaming account to get \"In Garage\" Tanks";
             this.badForm1.TitleHeight = 26;
+            // 
+            // pnlBrowser
+            // 
+            this.pnlBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlBrowser.BackColor = System.Drawing.Color.Transparent;
+            this.pnlBrowser.Location = new System.Drawing.Point(13, 64);
+            this.pnlBrowser.Name = "pnlBrowser";
+            this.pnlBrowser.Size = new System.Drawing.Size(580, 598);
+            this.pnlBrowser.TabIndex = 2;
             // 
             // txtAddress
             // 
@@ -72,34 +85,21 @@
             this.txtAddress.Name = "txtAddress";
             this.txtAddress.PasswordChar = '\0';
             this.txtAddress.ReadOnly = true;
-            this.txtAddress.Size = new System.Drawing.Size(792, 23);
+            this.txtAddress.Size = new System.Drawing.Size(580, 23);
             this.txtAddress.TabIndex = 1;
             this.txtAddress.Text = "Loading...";
             this.txtAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.txtAddress.ToolTipText = "Loading...";
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.webBrowser1.Location = new System.Drawing.Point(12, 63);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(793, 811);
-            this.webBrowser1.TabIndex = 0;
-            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
-            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
-            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
             // 
             // InGarageApi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Fuchsia;
-            this.ClientSize = new System.Drawing.Size(818, 886);
+            this.ClientSize = new System.Drawing.Size(606, 678);
             this.Controls.Add(this.badForm1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(380, 500);
             this.Name = "InGarageApi";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -113,8 +113,8 @@
 
 		#endregion
 
-		private BadForm badForm1;
-		private System.Windows.Forms.WebBrowser webBrowser1;
+        private BadForm badForm1;
 		private BadTextBox txtAddress;
+        private System.Windows.Forms.Panel pnlBrowser;
 	}
 }
