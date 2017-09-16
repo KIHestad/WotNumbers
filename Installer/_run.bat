0a112eae715d4a84e8197544e44cac7ac9ffb8ff
BadButton <kih@hestad.no>
2017-09-16 23:13:24 +0200

    Changed to new installer using Wix 3.11, with native support for VS 2017. Requre Wix Toolkit to be installed on PX: http://wixtoolset.org/releases/

--- Installer/_run.bat
+++
@@ -1,6 +0,0 @@
-echo BIN
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\heat.exe" dir ..\WinApp\Bin\Debug -gg -srd -dr INSTALLFOLDER_Bin -var var.BinSource -cg ProductComponents_Bin -o _Bin.wxs
-echo LIB
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\heat.exe" dir ..\WinApp\Lib -gg -srd -dr INSTALLFOLDER_Lib -var var.LibSource -cg ProductComponents_Lib -o _Lib.wxs
-echo D2J
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\heat.exe" dir ..\WinApp\Bin\Debug\Dossier2json -gg -srd -dr INSTALLFOLDER_Dossier2json -var var.D2jSource -cg ProductComponents_Dossier2json -o _D2j.wxs
