558cefc1176b7ead3c251f79af77c088d41585e1
BadButton <kih@hestad.no>
2018-03-26 04:27:29 +0200

    Fix BRR mod

--- WinApp/Properties/AssemblyInfo.cs
+++ WinApp/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // - Second number = minor versjon
 // - Third number = not used
 // - Forth number = buid number (automaically)
-[assembly: AssemblyVersion("0.20.7.*")]
-[assembly: AssemblyFileVersion("0.20.7.0")]
+[assembly: AssemblyVersion("0.20.8.*")]
+[assembly: AssemblyFileVersion("0.20.8.0")]
