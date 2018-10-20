5f443853c7203e1aab1aac33cc789352e33bb5ca
BadButton <kih@hestad.no>
2018-10-20 02:17:55 +0200

    Forgot version numbering on Python battle parser

--- WinApp/Dossier2json/wotbr2j.py
+++ WinApp/Dossier2json/wotbr2j.py
@@ -43,7 +43,7 @@ VEH_INTERACTION_DETAILS_TYPES = dict(((x[0], x[1]) for x in VEH_INTERACTION_DETA
   
   
 parser = dict()
-parser['version'] = "1.0.2.1"
+parser['version'] = "1.2.0.1"
 parser['name'] = 'http://wotnumbers.com'
 parser['processingTime'] = int(time.mktime(time.localtime()))
 cachefile = None #ironpython modified
