914b159e4af4373c6a27550c13d161f518d17b1c
D0ct0rDave <dementedworld@gmail.com>
2022-10-21 18:48:56 +0200

    Orphan Battles WIP 2

---
+++ WinApp/Code/BonusType.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinApp.Code
+{
+    class BonusType
+    {
+        public int bonusType { get; set; }
+
+        BonusType(int i_bonusType)
+        {
+            bonusType = i_bonusType;
+        }
+
+        public static implicit operator BonusType(int i_bonusType) => new BonusType(i_bonusType);
+        public static implicit operator int(BonusType i_bonusType) => i_bonusType.bonusType;
+        public static bool operator ==(BonusType a, BonusType b) => a.bonusType == b.bonusType;
+        public static bool operator !=(BonusType a, BonusType b) => a.bonusType != b.bonusType;
+
+        public bool GetPlatoon()
+        {
+            return bonusType == 1;
+        }
+
+        public bool GetFortResource()
+        {
+            return bonusType == 10;
+        }
+
+        public bool GetEnemyClan()
+        {
+            switch (bonusType)
+            {
+                case 3: // [fallthrough]
+                case 4: // [fallthrough]
+                case 5: // [fallthrough]
+                case 10: // [fallthrough]
+                case 11: // [fallthrough]
+                case 12: // [fallthrough]
+                case 13: return true;
+            }
+            return false;
+        }
+
+        public string GetBattleResultMode()
+        {
+            // Check class ARENA_BONUS_TYPE in constants.py
+            switch (bonusType)
+            {
+                case 0: return "Unknown";
+                case 1: return "Random";
+                case 2: return "Trainig Room";
+                case 3: return "Tank Company";
+                case 4: return "Tournament";
+                case 5: return "Clan War";
+                case 6: return "Tutorial";
+                case 7: return BattleMode.GetItemFromType(BattleMode.TypeEnum.ModeTeam).Name;
+                case 8: return BattleMode.GetItemFromType(BattleMode.TypeEnum.ModeHistorical).Name;
+                case 9: return BattleMode.GetItemFromType(BattleMode.TypeEnum.ModeSpecial).Name;
+                case 10: return BattleMode.GetItemFromType(BattleMode.TypeEnum.ModeSkirmishes).Name;
+                case 11: return BattleMode.GetItemFromType(BattleMode.TypeEnum.ModeStronghold).Name;
+                case 12: return BattleMode.GetItemFromType(BattleMode.TypeEnum.ModeTeamRanked).Name;
+                case 13: return BattleMode.GetItemFromType(BattleMode.TypeEnum.ModeGlobalMap).Name;
+                case 14: return "Tournament Regular";
+                case 15: return "Tournament Clan";
+                case 16: return "Rated Sandbox";
+                case 17: return "Sandbox";
+                case 18: return "Fallout";
+                case 19: return "Fallout Multiteam";
+                case 20: return "Sortie ";
+                case 21: return "Fort battle";
+                case 22: return "Ranked";
+                case 23: return "Bootcamp";
+                case 24: return BattleMode.GetItemFromType(BattleMode.TypeEnum.ModeGrand).Name;
+                case 25: return "Epic random training";
+                case 26: return "Event battles";
+                case 27: return "Epic battle ";
+                case 28: return "Epic battle training";
+                case 29: return "Battle Royale solo";
+                case 30: return "Battle Royale squad";
+                case 31: return "Tournament event";
+                case 32: return "Bob";
+                case 33: return "Event random";
+                case 34: return "Battle Royale Training Solo";
+                case 35: return "Battle Royale Training Squad";
+                case 36: return "Weekend brawl";
+                case 37: return "Map Box";
+                case 38: return "Map training";
+                case 39: return "Real Time Strategy";
+                case 40: return "Real Time Strategy 1x1";
+                case 41: return "Real Time Strategy Bootcamp";
+                case 42: return "Fun random";
+                case 43: return "Competitive 7: Onslaught";
+            }
+
+            return "Unknown";
+        }
+        public BattleMode.TypeEnum GetBattleMode()
+        {
+            switch (bonusType)
+            {
+                case 1: return BattleMode.TypeEnum.ModeRandom_TC;
+                case 7: return BattleMode.TypeEnum.ModeTeam;
+                case 8: return BattleMode.TypeEnum.ModeHistorical;
+                case 9: return BattleMode.TypeEnum.ModeSpecial;
+                case 10: return BattleMode.TypeEnum.ModeSkirmishes;
+                case 11: return BattleMode.TypeEnum.ModeStronghold;
+                case 12: return BattleMode.TypeEnum.ModeTeamRanked;
+                case 13: return BattleMode.TypeEnum.ModeGlobalMap;
+                case 24: return BattleMode.TypeEnum.ModeGrand;
+            }
+
+            return BattleMode.TypeEnum.AllModes;
+        }
+    }
+}
--- WinApp/Forms/Settings/RecalcBattleMinTier.cs
+++ WinApp/Forms/Settings/RecalcBattleMinTier.cs
@@ -12,20 +12,24 @@ using WinApp.Code;
 namespace WinApp.Forms
 {
 	public partial class RecalcBattleMinTier : FormCloseOnEsc
-    {
+	{
 		private static bool _autoRun = false;
-        public RecalcBattleMinTier(bool autoRun = false)
+		public RecalcBattleMinTier(bool autoRun = false)
 		{
 			InitializeComponent();
-			_autoRun = autoRun; 
+			_autoRun = autoRun;
 		}
 
-        private async void RecalcBattleMinTier_Shown(object sender, EventArgs e)
+		private async void RecalcBattleMinTier_Shown(object sender, EventArgs e)
 		{
 			if (_autoRun)
-                await RunNow();
+				await RunNow();
 		}
 
+		private string GetProcessingString()
+		{
+			return "Recalc Min Tier ";
+		}
 		private void UpdateProgressBar(string statusText, int count)
 		{
 			lblProgressStatus.Text = statusText;
@@ -43,58 +47,49 @@ namespace WinApp.Forms
 			btnStart.Enabled = false;
 			badProgressBar.Value = 0;
 			badProgressBar.Visible = true;
-            // Get battles
+			// Get battles
 			UpdateProgressBar("Getting battle count", 0);
-            // Credits = total income
-            // CreditsNet = total income - all cost pre calculated
-            string sql =
-                "SELECT battle.id as battleId, battle.battleTime as battleTime, min(tank.tier) as battleMinTier " +
-                "from battle " +  
-                " inner join battlePlayer on battle.id = battlePlayer.battleId  " +
-                " inner join tank on battlePlayer.tankId = tank.id " +
-                "group by battle.id, battle.battleTime " +
-                "ORDER BY battle.id";
-				
+
+			string sql = 
+				"SELECT battle.id as battleId, battle.battleTime as battleTime, min(tank.tier) as battleMinTier " +
+				"from battle " +
+				" inner join battlePlayer on battle.id = battlePlayer.battleId  " +
+				" inner join tank on battlePlayer.tankId = tank.id " +
+				"group by battle.id, battle.battleTime " +
+				"ORDER BY battle.id";
 			DataTable dt = await DB.FetchData(sql);
+
 			int tot = dt.Rows.Count;
 			badProgressBar.ValueMax = tot + 2;
 			sql = "";
-            int loopCount = 0;
-            string updateSQL = "";
-            string battleTime = "";
-            UpdateProgressBar("Starting updates...", 1);
+			int loopCount = 0;
+			string battleTime = "";
 
+			UpdateProgressBar("Starting updates...", 1);
 			foreach (DataRow dr in dt.Rows)
 			{
 				// Build SQL
-				updateSQL += "UPDATE battle SET minBattleTier=" + dr["battleMinTier"].ToString() + " WHERE id=" + dr["battleId"].ToString() + "; " + Environment.NewLine;
+				sql += "UPDATE battle SET minBattleTier=" + dr["battleMinTier"].ToString() + " WHERE id=" + dr["battleId"].ToString() + "; " + Environment.NewLine;
 				loopCount++;
-				if (loopCount >= 20)
+				if (loopCount >= Constants.RecalcDataBatchSize)
 				{
-					sql = updateSQL;
-
 					battleTime = dr["battleTime"].ToString();
-					UpdateProgressBar("Calc Min Tier " + badProgressBar.Value + "/" + tot.ToString() + " " + battleTime, loopCount);
-					await DB.ExecuteNonQuery(updateSQL, Config.Settings.showDBErrors, true);
+					UpdateProgressBar(GetProcessingString() + badProgressBar.Value + "/" + tot.ToString() + " " + battleTime, loopCount);
+					await DB.ExecuteNonQuery(sql, Config.Settings.showDBErrors, true);
 
 					loopCount = 0;
-					updateSQL = "";
+					sql = "";
 				}
 			}
-			if (updateSQL != "") // Update last batch of sql's
+			if (sql != "") // Update last batch of sql's
 			{
-				sql = updateSQL;
-
-				UpdateProgressBar("Calc Min Tier " + badProgressBar.Value + "/" + tot.ToString() + " " + battleTime, loopCount);
-				await DB.ExecuteNonQuery(updateSQL, Config.Settings.showDBErrors, true);
-
-				updateSQL = "";
+				UpdateProgressBar(GetProcessingString() + badProgressBar.Value + "/" + tot.ToString() + " " + battleTime, loopCount);
+				await DB.ExecuteNonQuery(sql, Config.Settings.showDBErrors, true);
+				sql = "";
 			}
 
-
-
 			// Done
-			UpdateProgressBar("",0);
+			UpdateProgressBar("", 0);
 			lblProgressStatus.Text = "Update finished: " + DateTime.Now.ToString();
 			btnStart.Enabled = true;
 
@@ -105,9 +100,9 @@ namespace WinApp.Forms
 
 		private async void btnStart_Click(object sender, EventArgs e)
 		{
-            await RunNow();
+			await RunNow();
 		}
 
-		
+
 	}
 }
