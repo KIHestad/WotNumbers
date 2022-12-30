a8288986b721fd0b2911a5014374b4bbbf0b5901
D0ct0rDave <dementedworld@gmail.com>
2022-12-30 17:30:03 +0100

    - Single WoT Numbers instance checking.

--- WinApp/Program.cs
+++ WinApp/Program.cs
@@ -4,7 +4,13 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;   //GuidAttribute
+using System.Reflection;                //Assembly
+using System.Threading;                 //Mutex
+using System.Security.AccessControl;    //MutexAccessRule
+using System.Security.Principal;        //SecurityIdentifier
 using WinApp.Code;
+
 
 namespace WinApp
 {
@@ -18,12 +24,25 @@ namespace WinApp
 		{
             try
             {
-                Application.EnableVisualStyles();
-				Application.SetCompatibleTextRenderingDefault(false);
-				Form mainForm = new Forms.Main();
-				// mainForm.FormBorderStyle = FormBorderStyle.None;
-				// mainForm.Visible = false;
-				Application.Run(mainForm);
+                if (Config.Settings.allowMultipleInstances)
+                {
+                    // Run no matter what.
+                    Run();
+                }
+                else 
+                { 
+                    String appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), true)[0]).Value;
+                    using (Mutex mutex = new Mutex(false, "Global\\" + appGuid))
+                    {
+                        if (!mutex.WaitOne(0, false))
+                        {
+                            MessageBox.Show("Another instance of WoT Numbers is already running");
+                            return;
+                        }
+
+                        Run();
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -42,6 +61,16 @@ namespace WinApp
                     icon: MessageBoxIcon.Error);
                 // throw;
             }
+        }
+
+        static void Run()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form mainForm = new Forms.Main();
+            // mainForm.FormBorderStyle = FormBorderStyle.None;
+            // mainForm.Visible = false;
+            Application.Run(mainForm);
         }
 	}
 }
